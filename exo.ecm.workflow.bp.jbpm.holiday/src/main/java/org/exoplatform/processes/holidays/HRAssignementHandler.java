6ce2648b3c256e56c1d3a86bc5e02d0c112a9d0f
Arnaud  Héritier <arnaud.heritier@exoplatform.com>
2010-03-16 20:34:23 +0000

    ECMS-91 : move all artifacts in flat mode

---
+++ exo.ecm.workflow.bp.jbpm.holiday/src/main/java/org/exoplatform/processes/holidays/HRAssignementHandler.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2003-2007 eXo Platform SAS.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see<http://www.gnu.org/licenses/>.
+ */
+
+package org.exoplatform.processes.holidays;
+
+import org.jbpm.graph.exe.ExecutionContext;
+import org.jbpm.taskmgmt.def.AssignmentHandler;
+import org.jbpm.taskmgmt.exe.Assignable;
+
+
+/**
+ * Created y the eXo platform team
+ * User: Benjamin Mestrallet
+ * Date: 13 mai 2004
+ */
+public class HRAssignementHandler implements AssignmentHandler{
+
+  public void assign(Assignable assignable, ExecutionContext executionContext) throws Exception {
+    assignable.setActorId("*:/organization/management/human-resources");
+  }
+
+}
