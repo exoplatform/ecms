8f4a9644cf5c96fb12db604ca16f9605377f89dd
Hoang Van Hung <hung.van@exoplatform.com>
2010-03-23 08:18:57 +0000

    ECMS-126  Merge/Split DMS portlet sub projects (MERGE-1)

---
+++ exo-ecms-apps-portlet-administration/src/main/java/org/exoplatform/ecm/webui/component/admin/namespace/UINamespaceManager.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (C) 2003-2007 eXo Platform SAS.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see<http://www.gnu.org/licenses/>.
+ */
+package org.exoplatform.ecm.webui.component.admin.namespace;
+
+
+import org.exoplatform.ecm.webui.component.admin.manager.UIAbstractManager;
+import org.exoplatform.webui.config.annotation.ComponentConfig;
+import org.exoplatform.webui.core.UIPopupWindow;
+import org.exoplatform.webui.core.lifecycle.UIContainerLifecycle;
+
+/**
+ * Created by The eXo Platform SARL
+ * Author : pham tuan
+ *          phamtuanchip@yahoo.de
+ * September 20, 2006
+ * 16:37:15 AM
+ */
+@ComponentConfig(lifecycle = UIContainerLifecycle.class)
+public class UINamespaceManager extends UIAbstractManager {
+
+  public UINamespaceManager() throws Exception {addChild(UINamespaceList.class, null, null) ;}
+  
+  public void refresh ()throws Exception {
+    UINamespaceList list = getChild(UINamespaceList.class) ;
+    list.updateGrid() ;
+  }
+  
+  public void initPopup() throws Exception {
+    UIPopupWindow uiPopup = getChild(UIPopupWindow.class) ;
+    if(uiPopup == null) {
+      uiPopup = addChild(UIPopupWindow.class, null, "NamespacePopup") ;
+      uiPopup.setWindowSize(600,0) ;
+      UINamespaceForm uiNamespaceForm = createUIComponent(UINamespaceForm.class, null, null) ;
+      uiPopup.setUIComponent(uiNamespaceForm) ;
+      uiPopup.setShow(true) ;
+      uiPopup.setResizable(true) ;
+      return ;
+    } 
+    uiPopup.setRendered(true) ;
+    uiPopup.setShow(true) ;
+  }
+}
