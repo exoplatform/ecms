42d76df5ba3edf9e6d989a5fd7f857cb5e358777
Phan Le Thanh Chuong <chuong.phan@exoplatform.com>
2010-04-07 08:46:43 +0000

    ECMS-28: Some content don't work in categories

--- exo-ecms-core-publication/src/main/java/org/exoplatform/services/wcm/publication/listener/post/PostCreateNodeTypeEventListener.java
+++ exo-ecms-core-publication/src/main/java/org/exoplatform/services/wcm/publication/listener/post/PostCreateNodeTypeEventListener.java
@@ -16,10 +16,21 @@
  */
 package org.exoplatform.services.wcm.publication.listener.post;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.jcr.Node;
+import javax.jcr.Session;
+import javax.jcr.Value;
+import javax.jcr.ValueFactory;
+
 import org.exoplatform.services.cms.CmsService;
+import org.exoplatform.services.cms.actions.ActionServiceContainer;
+import org.exoplatform.services.cms.taxonomy.TaxonomyService;
 import org.exoplatform.services.listener.Event;
 import org.exoplatform.services.listener.Listener;
 import org.exoplatform.services.wcm.publication.WCMComposer;
+import org.exoplatform.services.wcm.utils.WCMCoreUtils;
 
 /**
  * Created by The eXo Platform SAS
@@ -31,6 +42,10 @@ public class PostCreateNodeTypeEventListener extends Listener<CmsService, String
   
   /** The publication service. */
   private WCMComposer composer;
+  
+  private TaxonomyService taxonomyService;
+  
+  private ActionServiceContainer actionServiceContainer;
     
   /**
    * Instantiates a new post create content event listener.
@@ -48,5 +63,26 @@ public class PostCreateNodeTypeEventListener extends Listener<CmsService, String
    */
   public void onEvent(Event<CmsService, String> event) throws Exception {
 	  composer.cleanTemplates();
+	  
+	  // TODO: Hardcode for now, we need to improve the way to update affectedNodeTypeNames
+	  this.taxonomyService = WCMCoreUtils.getService(TaxonomyService.class);
+	  this.actionServiceContainer = WCMCoreUtils.getService(ActionServiceContainer.class);
+	  String nodetypeName = event.getData();
+	  String repository = WCMCoreUtils.getRepository(null).getConfiguration().getName();
+	  List<Node> taxonomyTrees = taxonomyService.getAllTaxonomyTrees(repository);
+	  for (Node taxonomyTree : taxonomyTrees) {
+		  Node action = actionServiceContainer.getAction(taxonomyTree, "taxonomyAction");
+		  Session session = action.getSession();
+		  ValueFactory valueFactory = session.getValueFactory();
+		  Value[] values = action.getProperty("exo:affectedNodeTypeNames").getValues();
+		  List<Value> tmpValues = new ArrayList<Value>();
+		  for (Value value : values) {
+			  tmpValues.add(value);
+		  }
+		  tmpValues.add(valueFactory.createValue(nodetypeName));
+		  action.setProperty("exo:affectedNodeTypeNames", tmpValues.toArray(new Value[tmpValues.size()]));
+		  session.save();
+		  session.logout();
+	  }
   }
 }
