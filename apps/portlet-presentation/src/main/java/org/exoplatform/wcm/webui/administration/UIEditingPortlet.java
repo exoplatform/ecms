4ed0df02e482219d9596a13ce3d1ada3bf0fb1c9
Benjamin  Paillereau <benjamin.paillereau@exoplatform.com>
2010-04-21 10:53:54 +0000

    ECMS-380 : multiple QuickEdit.js calls replaced by one call in EditingPortlet ; removed QuickEdit.js from gatein-resources

--- apps/portlet-presentation/src/main/java/org/exoplatform/wcm/webui/administration/UIEditingPortlet.java
+++ apps/portlet-presentation/src/main/java/org/exoplatform/wcm/webui/administration/UIEditingPortlet.java
@@ -1,7 +1,13 @@
 package org.exoplatform.wcm.webui.administration;
+import javax.portlet.MimeResponse;
+import javax.portlet.RenderResponse;
+
+import org.exoplatform.webui.application.WebuiApplication;
+import org.exoplatform.webui.application.WebuiRequestContext;
 import org.exoplatform.webui.config.annotation.ComponentConfig;
 import org.exoplatform.webui.core.UIPortletApplication;
 import org.exoplatform.webui.core.lifecycle.UIApplicationLifecycle;
+import org.w3c.dom.Element;
 
 /**
  * Created by The eXo Platform SAS Author : eXoPlatform
@@ -15,5 +21,18 @@ public class UIEditingPortlet extends UIPortletApplication {
 
   public UIEditingPortlet() throws Exception {
     addChild(UIEditingForm.class, null, null);
+  }
+  
+  /* (non-Javadoc)
+   * @see org.exoplatform.webui.core.UIPortletApplication#processRender(org.exoplatform.webui.application.WebuiApplication, org.exoplatform.webui.application.WebuiRequestContext)
+   */
+  public void processRender(WebuiApplication app, WebuiRequestContext context) throws Exception {
+	RenderResponse response = context.getResponse();
+	Element elementS = response.createElement("script");
+	elementS.setAttribute("type", "text/javascript");
+	elementS.setAttribute("src", "/eXoWCMResources/javascript/eXo/wcm/frontoffice/private/QuickEdit.js");
+	response.addProperty(MimeResponse.MARKUP_HEAD_ELEMENT,elementS); 
+
+    super.processRender(app, context) ;
   }
 }
