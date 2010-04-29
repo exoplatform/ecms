a5c6ef6c214676d3a51ece8532917ae8bee67966
Benjamin  Paillereau <benjamin.paillereau@exoplatform.com>
2010-04-29 10:51:23 +0000

    ECMS-430 : cronjob every 2 minutes

--- ext/authoring/services/src/main/java/org/exoplatform/wcm/extensions/component/rest/LifecycleConnector.java
+++ ext/authoring/services/src/main/java/org/exoplatform/wcm/extensions/component/rest/LifecycleConnector.java
@@ -1,8 +1,6 @@
 package org.exoplatform.wcm.extensions.component.rest;
 
-import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.HashMap;
 import java.util.List;
