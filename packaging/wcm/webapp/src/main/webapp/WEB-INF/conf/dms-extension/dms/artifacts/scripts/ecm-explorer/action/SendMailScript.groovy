84aab87080a690d855d74fbce8f28373ea5496c0
Phan Trong Lam <lam.phan@exoplatform.com>
2010-07-02 08:05:32 +0000

    ECMS-854 customize some information on sendmailscript.groovy

--- packaging/wcm/webapp/src/main/webapp/WEB-INF/conf/dms-extension/dms/artifacts/scripts/ecm-explorer/action/SendMailScript.groovy
+++ packaging/wcm/webapp/src/main/webapp/WEB-INF/conf/dms-extension/dms/artifacts/scripts/ecm-explorer/action/SendMailScript.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2003-2007 eXo Platform SAS.
+ * Copyright (C) 2003-2010 eXo Platform SAS.
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Affero General Public License
@@ -16,51 +16,53 @@
  */
 
 import java.util.Map;
+
 import javax.mail.Session;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 import javax.mail.internet.MimeMessage.RecipientType;
-import org.exoplatform.services.mail.MailService;
+
+import org.apache.commons.logging.Log;
 import org.exoplatform.container.PortalContainer;
 import org.exoplatform.services.cms.scripts.CmsScript;
-import org.exoplatform.services.jcr.RepositoryService;
+import org.exoplatform.services.log.ExoLogger;
+import org.exoplatform.services.mail.MailService;
 
-/*
-
-* Will need to get The MailService when it has been moved to exo-platform
-
-*/
 
 public class SendMailScript implements CmsScript {
-  private RepositoryService repositoryService_;
 
-  public SendMailScript(RepositoryService repositoryService) {
-    repositoryService_ = repositoryService;
+  private static Log LOG = ExoLogger.getLogger("ecm.SendMailScript");
+  private static String DEFAULT_MAIL = "exosender@gmail.com";
+  private static String DEFAULT_DESCRIPTION = "You have received a message from ";
+  
+  public SendMailScript() throws Exception {
   }
-
+  
   public void execute(Object context) {               
      Map variables = (Map) context;                      
-     String to = variables.get("exo:to");     
-     String nodePath = (String) variables.get("nodePath");
-   
+     String to = variables.get("exo:to").toString();     
+     String from = variables.get("exo:from").toString();
      try {
-    	 String nodeName = nodePath.split("/")[nodePath.split("/").size() -1]
-       String subject = variables.get("actionName");              
-       String message = variables.get("exo:description");
+       String subject = variables.get("actionName").toString();              
+       String message = variables.get("exo:description").toString();
        MailService service = (MailService)PortalContainer.getComponent(MailService.class);
        Session mailSession = service.getMailSession();
        MimeMessage msg = new MimeMessage(mailSession);
-       msg.setFrom(new InternetAddress("alerte@secours-catholique.org"));
+       if(from.equals("null") || from.isEmpty()) {
+         from = DEFAULT_MAIL;
+       }
+     if(message.equals("null") || message.isEmpty()) {
+       message = DEFAULT_DESCRIPTION + from;
+     }
+       msg.setFrom(new InternetAddress(from));
        msg.setRecipient(RecipientType.TO, new InternetAddress(to));
+     
        msg.setSubject(subject);
        msg.setContent(message, "text/html ; charset=ISO-8859-1");
        service.sendMessage(msg);
-       println("Send message in SendMailScript from " + msg.getFrom() + "to " + variables.get("exo:to"));
+       LOG.info("Message was sent from " + from + " to " + to);
      } catch (Exception e) {
-         if (session != null) {
-       		 session.logout();
-         }
-         e.printStackTrace();
+       LOG.error("Message sending failed", e);
      }
   }
 
