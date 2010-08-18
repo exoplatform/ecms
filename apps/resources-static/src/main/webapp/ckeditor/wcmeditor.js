9700f8fab3791d9076ebd2df3182807d6d7c57ce
Lam Tuan Nguyen <lam.nguyen@exoplatform.com>
2010-08-18 04:50:46 +0000

    ECMS-1229, update CKEDITOR from version 3.2 to 3.3.2

---
+++ apps/resources-static/src/main/webapp/ckeditor/wcmeditor.js
@@ -0,0 +1,21 @@
+﻿// Include ckeditor script.
+function inc(filename)
+{
+var body = document.getElementsByTagName('body').item(0);
+script = document.createElement('script');
+script.src = filename;
+script.type = 'text/javascript';
+body.appendChild(script)
+}
+
+inc("/eXoStaticResources/ckeditor/ckeditor.js");
+
+function updateck() {
+    for ( instance in CKEDITOR.instances ) {
+      if (document.getElementById(instance)==null) { clearInterval(intckeditor); }
+      document.getElementById(instance).value = CKEDITOR.instances[instance].getData();
+    }
+  }
+
+// This code is mandatory to update CKEditor instances as they don't sync natively in Ajax Popup with Chrome and Safari Browser
+var intckeditor = setInterval("updateck()", 1000);
