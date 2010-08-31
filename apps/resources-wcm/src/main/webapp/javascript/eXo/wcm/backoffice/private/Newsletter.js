377cfbafccf9576df241a8e6b1d3cf9f96ca4824
Phan Trong Lam <lam.phan@exoplatform.com>
2010-08-31 07:15:44 +0000

    ECMS-1370 Menu display without interlace

--- apps/resources-wcm/src/main/webapp/javascript/eXo/wcm/backoffice/private/Newsletter.js
+++ apps/resources-wcm/src/main/webapp/javascript/eXo/wcm/backoffice/private/Newsletter.js
@@ -23,4 +23,35 @@ NewsletterManager.prototype.checkBeforeDelete = function(uiSubscriptionsForm, ch
 	return confirm(confirmMess);
 }
 
+NewsletterManager.prototype.hide = function() {
+	var ln = eXo.core.DOMUtil.hideElementList.length ;
+	if (ln > 0) {
+		for (var i = 0 ; i < ln ; i++) {
+			eXo.core.DOMUtil.hideElementList[i].style.display = "none" ;
+		}
+	}
+} ;
+
+NewsletterManager.prototype.show = function(obj, evt) {
+		if(!evt) evt = window.event ;
+		evt.cancelBubble = true ;
+		var DOMUtil = eXo.core.DOMUtil ;
+		var uiPopupCategory = DOMUtil.findFirstDescendantByClass(obj, 'div', 'UIPopupCategory') ;	
+		if (!uiPopupCategory) return ;	
+		if(uiPopupCategory.style.display == "none") {
+				eXo.ecm.NewsletterManager.hide();
+				if (eXo.core.Browser.isIE7()) {	
+						uiPopupCategory.style.left = "-176px";
+			  	uiPopupCategory.style.top = "16px";
+						uiPopupCategory.style.display = "block";			
+		  }
+		  else {
+		  		uiPopupCategory.style.display = "block";
+		  }
+				eXo.core.DOMUtil.listHideElements(uiPopupCategory);
+		}	
+		else uiPopupCategory.style.display = "none" ;
+}
+
+
 eXo.ecm.NewsletterManager = new NewsletterManager();
