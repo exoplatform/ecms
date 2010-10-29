17f37b3c85815e148dfe9901bb6abd633f2e21fb
Vu Anh Nguyen <vu.nguyen@exoplatform.com>
2010-10-29 09:58:35 +0000

    ECMS-1664 change the Log class to correct one

--- packaging/wcm/webapp/src/main/webapp/WEB-INF/conf/dms-extension/dms/artifacts/scripts/ecm-explorer/action/SendMailScript.groovy
+++ packaging/wcm/webapp/src/main/webapp/WEB-INF/conf/dms-extension/dms/artifacts/scripts/ecm-explorer/action/SendMailScript.groovy
@@ -22,11 +22,14 @@ import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 import javax.mail.internet.MimeMessage.RecipientType;
 
-import org.apache.commons.logging.Log;
+import org.exoplatform.container.ExoContainer;
+import org.exoplatform.container.ExoContainerContext;
 import org.exoplatform.container.PortalContainer;
 import org.exoplatform.services.cms.scripts.CmsScript;
 import org.exoplatform.services.log.ExoLogger;
+import org.exoplatform.services.log.Log;
 import org.exoplatform.services.mail.MailService;
+
 
 
 public class SendMailScript implements CmsScript {
@@ -45,7 +48,8 @@ public class SendMailScript implements CmsScript {
      try {
        String subject = variables.get("actionName").toString();              
        String message = variables.get("exo:description").toString();
-       MailService service = (MailService)PortalContainer.getComponent(MailService.class);
+       ExoContainer myContainer = ExoContainerContext.getCurrentContainer();       
+       MailService service = (MailService)myContainer.getComponentInstanceOfType(MailService.class);
        Session mailSession = service.getMailSession();
        MimeMessage msg = new MimeMessage(mailSession);
        if(from.equals("null") || from.isEmpty()) {
