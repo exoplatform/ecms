13872c89a4eade079474610e82b77bba08e76d76
Vu Anh Nguyen <vu.nguyen@exoplatform.com>
2010-11-23 12:39:19 +0000

    ECMS-1722 Update the permission for all symlinks of a node

--- core/webui-explorer/src/main/java/org/exoplatform/ecm/webui/component/explorer/popup/info/UIPermissionForm.java
+++ core/webui-explorer/src/main/java/org/exoplatform/ecm/webui/component/explorer/popup/info/UIPermissionForm.java
@@ -23,6 +23,12 @@ import java.util.List;
 
 import javax.jcr.AccessDeniedException;
 import javax.jcr.Node;
+import javax.jcr.NodeIterator;
+import javax.jcr.Session;
+import javax.jcr.query.Query;
+import javax.jcr.query.QueryManager;
+import javax.jcr.query.QueryResult;
+
 
 import org.exoplatform.services.log.Log;
 import org.exoplatform.ecm.permission.info.UIPermissionInputSet;
@@ -33,10 +39,13 @@ import org.exoplatform.ecm.webui.selector.UISelectable;
 import org.exoplatform.ecm.webui.utils.PermissionUtil;
 import org.exoplatform.ecm.webui.utils.Utils;
 import org.exoplatform.services.cms.link.LinkManager;
+import org.exoplatform.services.cms.link.LinkUtils;
 import org.exoplatform.services.jcr.access.AccessControlEntry;
 import org.exoplatform.services.jcr.access.PermissionType;
 import org.exoplatform.services.jcr.access.SystemIdentity;
 import org.exoplatform.services.jcr.core.ExtendedNode;
+import org.exoplatform.services.jcr.core.ManageableRepository;
+import org.exoplatform.services.jcr.ext.common.SessionProvider;
 import org.exoplatform.services.log.ExoLogger;
 import org.exoplatform.services.organization.MembershipHandler;
 import org.exoplatform.services.organization.OrganizationService;
@@ -275,11 +284,12 @@ public class UIPermissionForm extends UIForm implements UISelectable {
         event.getRequestContext().addUIComponentToUpdateByAjax(uiApp.getUIPopupMessages());
         return;
       }
-      Node realNode = uiExplorer.getRealCurrentNode();
       LinkManager linkManager = uiExplorer.getApplicationComponent(LinkManager.class);
-      if (linkManager.isLink(realNode)) {
-        // Reset the permissions
-        linkManager.updateLink(realNode, currentNode);
+      List<Node> symlinks = LinkUtils.getAllSymlinks(currentNode, uiExplorer.getRepositoryName());
+      for (Node symlink : symlinks) {
+        try {
+          linkManager.updateLink(symlink, currentNode);
+        } catch (Exception e) {}
       }
       currentNode.getSession().save();
       uiForm.refresh();
