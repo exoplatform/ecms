dd9d196927664891c625e2412e11d1ed4e333f88
Minh Van Dang <minh.dang@exoplatform.com>
2010-11-23 09:12:33 +0000

    ECMS-1779 All templates content has been reset after restart server

--- core/services/src/main/java/org/exoplatform/services/cms/templates/impl/TemplatePlugin.java
+++ core/services/src/main/java/org/exoplatform/services/cms/templates/impl/TemplatePlugin.java
@@ -22,6 +22,7 @@ import java.util.Iterator;
 import java.util.List;
 
 import javax.jcr.Node;
+import javax.jcr.PathNotFoundException;
 import javax.jcr.PropertyType;
 import javax.jcr.RepositoryException;
 import javax.jcr.Session;
@@ -309,11 +310,25 @@ public class TemplatePlugin extends BaseComponentPlugin {
     for (Iterator iterator = templates.iterator(); iterator.hasNext();) {
       TemplateConfig.Template template = (TemplateConfig.Template) iterator.next();
       String templateFileName = template.getTemplateFile();
-      String path = basePath + templateFileName;            
+      String path = basePath + templateFileName; 
       InputStream in = configManager_.getInputStream(path);
       String nodeName = templateFileName.substring(templateFileName.lastIndexOf("/") + 1, templateFileName.indexOf("."));
-      templateService.addTemplate(templateType, nodeType.getNodetypeName(), nodeType.getLabel(), nodeType.getDocumentTemplate(), nodeName, 
-          template.getParsedRoles(), in, repository, templatesHome);
+      Node nodeTypeHome = null;
+      if (!templatesHome.hasNode(nodeType.getNodetypeName())) {
+        nodeTypeHome = Utils.makePath(templatesHome, nodeType.getNodetypeName(), NT_UNSTRUCTURED);
+      } else {
+        nodeTypeHome = templatesHome.getNode(nodeType.getNodetypeName());
+      }
+      Node specifiedTemplatesHome = null;
+      try {
+        specifiedTemplatesHome = nodeTypeHome.getNode(templateType);
+      } catch(PathNotFoundException e) {
+        specifiedTemplatesHome = Utils.makePath(nodeTypeHome, templateType, NT_UNSTRUCTURED);
+      }
+      if(!specifiedTemplatesHome.hasNode(nodeName)) {
+        templateService.addTemplate(templateType, nodeType.getNodetypeName(), nodeType.getLabel(), nodeType.getDocumentTemplate(), nodeName, 
+            template.getParsedRoles(), in, repository, templatesHome);
+      }
     }
   }    
   
