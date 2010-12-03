d88345bb93baed9342a6050cf0d87020b414c855
Benjamin  Paillereau <benjamin.paillereau@exoplatform.com>
2010-12-03 09:38:50 +0000

    ECMS-1833 : Update ProductVersions

--- core/services/src/main/java/org/exoplatform/ecm/ProductVersions.java
+++ core/services/src/main/java/org/exoplatform/ecm/ProductVersions.java
@@ -3,10 +3,11 @@ package org.exoplatform.ecm;
 public class ProductVersions {
 
 	public final static String WCM_2_0_0 = "WCM-2.0.0";
-	public final static String WCM_2_1_0 = "WCM-2.1.0";
-	
+  public final static String WCM_2_1_0 = "WCM-2.1.0";
+  public final static String WCM_2_1_2 = "WCM-2.1.2";
+
 	public static String getCurrentVersion() {
-		return WCM_2_1_0;
+		return WCM_2_1_2;
 	}
 
 }
