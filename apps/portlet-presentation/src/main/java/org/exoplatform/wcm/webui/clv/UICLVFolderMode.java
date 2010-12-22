c565a61cddeacd4c4b7280fcf76b4f6781568bf4
Tu Ngoc Nguyen <tu.nguyenngoc@exoplatform.com>
2010-12-22 08:10:33 +0000

    ECMS-1863 IE7: Exception when select 1 category with name contains accented

--- apps/portlet-presentation/src/main/java/org/exoplatform/wcm/webui/clv/UICLVFolderMode.java
+++ apps/portlet-presentation/src/main/java/org/exoplatform/wcm/webui/clv/UICLVFolderMode.java
@@ -115,9 +115,8 @@ public class UICLVFolderMode extends UICLVContainer {
     }      
     NodeLocation nodeLocation = NodeLocation.getNodeLocationByExpression(
     		(folderPath != null) ? folderPath : preferences.getValue(UICLVPortlet.PREFERENCE_ITEM_PATH, null));
-    String nPath  = new String(nodeLocation.getPath().getBytes("ISO-8859-1"), "UTF-8");
     return wcmComposer.getContents(nodeLocation.getRepository(), Text.escapeIllegalJcrChars(nodeLocation.getWorkspace()), 
-    		Text.escapeIllegalJcrChars(nPath), filters, WCMCoreUtils.getUserSessionProvider());
+        Text.escapeIllegalJcrChars(nodeLocation.getPath()), filters, WCMCoreUtils.getUserSessionProvider());
   }
   /**
 	 * Gets the bar info show.
