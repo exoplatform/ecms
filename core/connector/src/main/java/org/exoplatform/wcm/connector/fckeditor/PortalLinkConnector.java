936da3c08ba9e48fee9e2d813873361b8a7d4b41
Huong Thien Nguyen <huongnt@exoplatform.com>
2011-06-02 04:03:26 +0000

    ECMS-2089: Impossible to select a not visible page when creating a portal page link into a text (FCK Editor)

--- core/connector/src/main/java/org/exoplatform/wcm/connector/fckeditor/PortalLinkConnector.java
+++ core/connector/src/main/java/org/exoplatform/wcm/connector/fckeditor/PortalLinkConnector.java
@@ -299,10 +299,6 @@ public class PortalLinkConnector implements ResourceContainer {
                                Element filesElement,
                                String userId,
                                UserPortalConfigService portalConfigService) throws Exception {
-    if (!pageNode.isDisplay()) {
-      return;
-    }
-    
     String pageId = pageNode.getPageReference();
     Page page = portalConfigService.getPage(pageId, userId);
     String pageUri = "";
