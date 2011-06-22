bd8a4c48bc058bef94aa053df89ef6ca1cb3463b
Ha Viet Dang <ha.dangviet@exoplatform.com>
2011-06-22 09:18:27 +0000

    ECMS-2092 Unknown error when search with * while configuring CLV

--- core/webui/src/main/java/org/exoplatform/wcm/webui/selector/content/UIContentSearchForm.java
+++ core/webui/src/main/java/org/exoplatform/wcm/webui/selector/content/UIContentSearchForm.java
@@ -101,7 +101,7 @@ public class UIContentSearchForm extends UIForm {
     addUIFormInput(new UIFormStringInput(SEARCH_BY_NAME,SEARCH_BY_NAME,null));
     addUIFormInput(new UIFormStringInput(SEARCH_BY_CONTENT, SEARCH_BY_CONTENT, null));
 
-    addUIFormInput(new UIFormStringInput(PROPERTY, PROPERTY, null));
+    addUIFormInput(new UIFormStringInput(PROPERTY, PROPERTY, null).setEditable(false));
     addUIFormInput(new UIFormStringInput(CONTAIN, CONTAIN, null));
 
     List<SelectItemOption<String>> dateOptions = new ArrayList<SelectItemOption<String>>();
@@ -112,8 +112,7 @@ public class UIContentSearchForm extends UIForm {
     addUIFormInput(startTime);
     UIFormDateTimeInput endTime = new UIFormDateTimeInput(END_TIME, END_TIME, null, true);
     addUIFormInput(endTime);
-    addUIFormInput(new UIFormStringInput(DOC_TYPE, DOC_TYPE, null));
-//  addUIFormInput(new UIFormStringInput(CATEGORY, CATEGORY, null));
+    addUIFormInput(new UIFormStringInput(DOC_TYPE, DOC_TYPE, null).setEditable(false));
 
     setActions(new String[] {"SearchWebContent"} );
   }
@@ -279,6 +278,10 @@ public class UIContentSearchForm extends UIForm {
   }
 
   static public class SearchWebContentActionListener extends EventListener<UIContentSearchForm> {
+    
+    private static final String[] FILTER_CHARS = { "&", "$", "@", ":", "]", "[", "*", "%", "!", "", "(",
+                                           ")", "'", "#", ";", "}", "{", "/", "|", "\"" };
+    
     public void execute(Event<UIContentSearchForm> event) throws Exception {
       UIContentSearchForm uiWCSearch = event.getSource();
       String typeSearch = uiWCSearch.getAncestorOfType(UIContentSelector.class)
@@ -294,6 +297,18 @@ public class UIContentSearchForm extends UIForm {
       if(typeSearch.equals(UIContentBrowsePanel.WEBCONTENT) || typeSearch.equals(UIContentBrowsePanel.MEDIA)){
         if(UIContentSearchForm.SEARCH_BY_NAME.equals(radioValue)) {
           String keyword = uiWCSearch.getUIStringInput(radioValue).getValue();
+          if (keyword != null && keyword.length() > 0) {
+
+            for (String filterChar : FILTER_CHARS) {
+              if (keyword.indexOf(filterChar) > -1) {
+                uiApp.addMessage(new ApplicationMessage("UIContentSearchForm.msg.name-not-allowed",
+                                                        null,
+                                                        ApplicationMessage.WARNING));
+                event.getRequestContext().addUIComponentToUpdateByAjax(uiApp.getUIPopupMessages());
+                return;
+              }
+            }
+          }         
           if(uiWCSearch.haveEmptyField(uiApp, event, keyword)) return;
           pagResult = uiWCSearch.searchWebContentByName(keyword.trim(), qCriteria, pageSize);
         } else if(UIContentSearchForm.SEARCH_BY_CONTENT.equals(radioValue)) {
@@ -354,6 +369,17 @@ public class UIContentSearchForm extends UIForm {
       }else if(typeSearch.equals(UIContentBrowsePanel.DMSDOCUMENT)){
         if(UIContentSearchForm.SEARCH_BY_NAME.equals(radioValue)) {
           String keyword = uiWCSearch.getUIStringInput(radioValue).getValue();
+          if (keyword != null && keyword.length() > 0) {
+            for (String filterChar : FILTER_CHARS) {
+              if (keyword.indexOf(filterChar) > -1) {
+                uiApp.addMessage(new ApplicationMessage("UIContentSearchForm.msg.name-not-allowed",
+                                                        null,
+                                                        ApplicationMessage.WARNING));
+                event.getRequestContext().addUIComponentToUpdateByAjax(uiApp.getUIPopupMessages());
+                return;
+              }
+            }
+          }         
           if(uiWCSearch.haveEmptyField(uiApp, event, keyword)) return;
           pagResult = uiWCSearch.searchDocumentByName(keyword.trim(), qCriteria, pageSize);
         } else if(UIContentSearchForm.SEARCH_BY_CONTENT.equals(radioValue)) {
