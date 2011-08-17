362714283cc899acd0b7e9ad7c25ca9707222e38
Hai Thanh Le <hai.lethanh@exoplatform.com>
2011-08-17 02:53:40 +0000

    ECMS-2538  Show the document form when just have only one document type

--- core/webui-explorer/src/main/java/org/exoplatform/ecm/webui/component/explorer/popup/actions/UIDocumentFormController.java
+++ core/webui-explorer/src/main/java/org/exoplatform/ecm/webui/component/explorer/popup/actions/UIDocumentFormController.java
@@ -122,7 +122,20 @@ public class UIDocumentFormController extends UIContainer implements UIPopupComp
     }
     if(templates.size()>0) {
       UISelectDocumentForm uiSelectForm = getChild(UISelectDocumentForm.class) ;
-      uiSelectForm.setDocumentTemplates(templates);
+      if (templates.size() > 1) {        
+        uiSelectForm.setDocumentTemplates(templates);
+      } else {
+        UIDocumentFormController uiDCFormController = uiSelectForm.getParent() ;            
+        UIDocumentForm documentForm = uiDCFormController.getChild(UIDocumentForm.class) ;
+        documentForm.addNew(true);      
+        documentForm.getChildren().clear() ;
+        documentForm.resetInterceptors();
+        documentForm.resetProperties();            
+        documentForm.setContentType(templates.values().iterator().next());
+        documentForm.setCanChangeType(false);
+        uiSelectForm.setRendered(false);
+        documentForm.setRendered(true);
+      }
     }
   }
 
