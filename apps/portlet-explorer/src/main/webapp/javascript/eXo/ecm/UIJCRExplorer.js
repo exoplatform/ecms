0660b79013ee9b78c2a45f0021c5b5140c584760
Pham Duy Dong <dongpd@exoplatform.com>
2011-09-30 04:42:05 +0000

    ECMS-2680  View node type shows not good

--- apps/portlet-explorer/src/main/webapp/javascript/eXo/ecm/UIJCRExplorer.js
+++ apps/portlet-explorer/src/main/webapp/javascript/eXo/ecm/UIJCRExplorer.js
@@ -109,4 +109,46 @@ UIJCRExplorer.prototype.dropDownIconList = function(uniqueId) {
 	}
 };
 
+/**
+ * Gets the tab element and the tab content associated and displays them
+ *  . changes the style of the tab
+ *  . displays the tab content of the selected tab (display: block)
+ * if tabId are provided, can get the tab content by Ajax
+ */
+UIJCRExplorer.prototype.displayTabContent = function(clickedEle) {
+  var DOMUtil = eXo.core.DOMUtil;
+  var uiSelectTab = DOMUtil.findAncestorByClass(clickedEle, "UITab") ;
+
+  var uiHorizontalTabs = DOMUtil.findAncestorByClass(clickedEle, "UIHorizontalTabs") ;
+  var uiTabs = eXo.core.DOMUtil.findDescendantsByClass(uiHorizontalTabs, "li", "UITab") ;
+  var parentdHorizontalTab = uiHorizontalTabs.parentNode ;
+  var contentTabContainer = DOMUtil.findFirstDescendantByClass(parentdHorizontalTab, "div", "UITabContentContainer") ;
+  var uiTabContents = DOMUtil.findChildrenByClass(contentTabContainer, "div", "UITabContent") ;
+    var form = DOMUtil.getChildrenByTagName(contentTabContainer, "form") ;
+    if(form.length > 0) {
+        var tmp = DOMUtil.findChildrenByClass(form[0], "div", "UITabContent") ;
+    for(var i = 0; i < tmp.length; i++) {
+        uiTabContents.push(tmp[i]) ;
+    }
+    }
+  var index = 0 ;
+  for(var i = 0; i < uiTabs.length; i++) {
+    var styleTabDiv = DOMUtil.getChildrenByTagName(uiTabs[i], "div")[0] ;
+    if(styleTabDiv.className == "DisabledTab") continue ;
+    if(uiSelectTab == uiTabs[i]) {
+      styleTabDiv.className = "SelectedTab" ;
+      index = i ;
+            continue ;
+    }
+    styleTabDiv.className = "NormalTab" ;
+    uiTabContents[i].style.display = "none" ;
+  }
+  uiTabContents[index].style.display = "block" ;
+    if (eXo.ecm.UIJCRExplorer) {
+        try {
+                eXo.ecm.UIJCRExplorer.initViewNodeScroll();
+        } catch(e) {void(0);}
+    }
+};
+
 eXo.ecm.UIJCRExplorer = new UIJCRExplorer();
