31653afb5046a4344266cb7aec65ec9887a80c91
anhvurz90 <anhvurz90@gmail.com>
2012-05-09 15:53:38 +0700

    ECMS-3660 get session of node to save instead of creating new session

--- core/services/src/main/java/org/exoplatform/services/cms/templates/impl/TemplateServiceImpl.java
+++ core/services/src/main/java/org/exoplatform/services/cms/templates/impl/TemplateServiceImpl.java
@@ -998,13 +998,13 @@ public class TemplateServiceImpl implements TemplateService, Startable {
     String templatePath = null;
     try {
       templatePath = templatesHome.getPath() + "/" + nodeTypeName + "/" + templateType + "/" + templateName;
-      Node templateNode = (Node)getSession(WCMCoreUtils.getSystemSessionProvider()).getItem(templatePath);
+      Node templateNode = (Node)templatesHome.getSession().getItem(templatePath);
       updateTemplate(templateNode,templateFile, roles);
       templateNode.save();
     } catch(PathNotFoundException e) {
       templatePath = getContentNode(templateType, templatesHome, nodeTypeName, label,
           isDocumentTemplate, templateName, roles, templateFile);
-    } 
+    }
     // Update managedDocumentTypesMap
     removeCacheTemplate(templatePath);
     removeTemplateNodeTypeList();
@@ -1047,7 +1047,7 @@ public class TemplateServiceImpl implements TemplateService, Startable {
       resourceNode.setProperty(NodetypeConstant.JCR_LAST_MODIFIED, new GregorianCalendar());
       resourceNode.setProperty(NodetypeConstant.JCR_DATA, data);
       resourceNode.setProperty(NodetypeConstant.EXO_ROLES, roles);
-      getSession(WCMCoreUtils.getSystemSessionProvider()).save();
+      resourceNode.getSession().save();
       return contentNode.getPath();
     } catch (Exception e) {
       if (LOG.isErrorEnabled()) {
@@ -1066,7 +1066,7 @@ public class TemplateServiceImpl implements TemplateService, Startable {
       resourceNode.setProperty(NodetypeConstant.EXO_ROLES, roles);
       resourceNode.setProperty(NodetypeConstant.JCR_LAST_MODIFIED, new GregorianCalendar());
       resourceNode.setProperty(NodetypeConstant.JCR_DATA, data);
-      getSession(WCMCoreUtils.getSystemSessionProvider()).save();
+      resourceNode.getSession().save();
       return template.getPath();
     } catch (Exception e) {
       if (LOG.isErrorEnabled()) {
