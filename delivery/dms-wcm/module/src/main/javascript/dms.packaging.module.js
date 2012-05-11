502dd0249a04bd6d519b76dbbd98a3d4a1ede4d9
ttthanh <thanhtt@exoplatform.com>
2012-05-11 11:30:47 +0700

    PLF-3187: Update pdfbox version to 1.1.0-eXo01
    Fix description:
    	- Remove pdfbox dependency, use pdfbox packages from eXo core and upgrade pdfbox version in xcmis extension

--- delivery/dms-wcm/module/src/main/javascript/dms.packaging.module.js
+++ delivery/dms-wcm/module/src/main/javascript/dms.packaging.module.js
@@ -33,7 +33,6 @@ function getModule(params) {
   //addDependency(new Project("com.sun.xml.stream", "sjsxp", "jar", "1.0")).
     addDependency(new Project("org.icepdf", "icepdf-core", "jar", "4.3.2")).
     addDependency(new Project("org.icepdf", "icepdf-viewer", "jar", "4.3.2")).
-    addDependency(new Project("org.apache.pdfbox", "fontbox", "jar", "1.1.0")).
     addDependency(new Project("com.artofsolving", "jodconverter", "jar", "2.2.2")).
     addDependency(new Project("com.artofsolving", "jodconverter-cli", "jar", "2.2.2")).
     addDependency(new Project("org.openoffice", "ridl", "jar", "3.0.1")).
