c43c2317d670bc74e128b228bf1e24a24220bdb8
cuonglh <cuonglh@exoplatform.com>
2012-11-08 10:33:53 +0700

    ECMS-4181:Unknow error when try to publish image
    Fix description: Disable Publish, Aprove, RequestApproval button for subnode of webcontent.

--- ext/authoring/webui/src/main/java/org/exoplatform/wcm/extensions/ui/PublicationRequestApprovalAction.java
+++ ext/authoring/webui/src/main/java/org/exoplatform/wcm/extensions/ui/PublicationRequestApprovalAction.java
@@ -27,6 +27,7 @@ import org.exoplatform.ecm.webui.component.explorer.UIJCRExplorer;
 import org.exoplatform.ecm.webui.component.explorer.control.filter.CanAddNodeFilter;
 import org.exoplatform.ecm.webui.component.explorer.control.filter.IsCheckedOutFilter;
 import org.exoplatform.ecm.webui.component.explorer.control.filter.IsNotLockedFilter;
+import org.exoplatform.ecm.webui.component.explorer.control.filter.IsNotIgnoreVersionNodeFilter;
 import org.exoplatform.ecm.webui.component.explorer.control.listener.UIActionBarActionListener;
 import org.exoplatform.services.ecm.publication.PublicationService;
 import org.exoplatform.webui.config.annotation.ComponentConfig;
@@ -49,7 +50,7 @@ public class PublicationRequestApprovalAction extends UIComponent {
 
   private static final List<UIExtensionFilter> FILTERS = Arrays.asList(new UIExtensionFilter[] {
       new CanAddNodeFilter(), new IsNotLockedFilter(), new IsCheckedOutFilter(),
-      new CanValidateFilter()                         });
+      new CanValidateFilter(), new IsNotIgnoreVersionNodeFilter() });
 
   @UIExtensionFilters
   public List<UIExtensionFilter> getFilters() {
