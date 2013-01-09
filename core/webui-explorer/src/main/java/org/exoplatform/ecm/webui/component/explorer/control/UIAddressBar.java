b48c4240137715b1b31bd13465fd4a9483e3db0b
haquangtan <tanhq@exoplatform.com>
2013-01-09 17:43:20 +0700

    ECMS-4340: Content Administration redesign

--- core/webui-explorer/src/main/java/org/exoplatform/ecm/webui/component/explorer/control/UIAddressBar.java
+++ core/webui-explorer/src/main/java/org/exoplatform/ecm/webui/component/explorer/control/UIAddressBar.java
@@ -318,5 +318,22 @@ public class UIAddressBar extends UIForm {
       uiApp.addMessage(new ApplicationMessage(mess, null, ApplicationMessage.INFO)) ;
     }
   }
+  
+  public static String UppercaseFirstLetters(String str) 
+  {
+    boolean prevWasWhiteSp = true;
+    char[] chars = str.toCharArray();
+    for (int i = 0; i < chars.length; i++) {
+      if (Character.isLetter(chars[i])) {
+        if (prevWasWhiteSp) {
+          chars[i] = Character.toUpperCase(chars[i]);    
+        }
+        prevWasWhiteSp = false;
+      } else {
+        prevWasWhiteSp = Character.isWhitespace(chars[i]);
+      }
+    }
+    return new String(chars);
+  }
 
 }
