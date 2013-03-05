e7bd68dca444f48d7b50ba9434dc8d257dce1ffe
luong trung hieu <hieult2@exoplatform.com>
2013-03-05 10:39:57 +0700

    ECMS-4711 Incorrect UIs of Manage Metadata

--- core/webui-administration/src/main/java/org/exoplatform/ecm/webui/component/admin/metadata/UIMetadataManager.java
+++ core/webui-administration/src/main/java/org/exoplatform/ecm/webui/component/admin/metadata/UIMetadataManager.java
@@ -55,7 +55,7 @@ public class UIMetadataManager extends UIAbstractManager {
     removeChildById(METADATA_POPUP) ;
     UIPopupWindow uiPopup = addChild(UIPopupWindow.class, null, METADATA_POPUP);
     uiPopup.setShowMask(true);
-    uiPopup.setWindowSize(650, 150);
+    uiPopup.setWindowSize(650, 220);
     UIMetadataForm uiMetaForm = createUIComponent(UIMetadataForm.class, null, null) ;
     uiPopup.setUIComponent(uiMetaForm) ;
     uiPopup.setRendered(true);
