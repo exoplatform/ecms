45d8cb14531c8fa57b416ffd805e7f503424167e
maydt <maydt@exoplatformm.com>
2013-03-05 14:54:09 +0700

    ECMS-4722: Improve NodePathUpgradePlugin

--- upgrade/templates/src/main/java/org/exoplatform/ecms/upgrade/plugins/NodePathUpgradePlugin.java
+++ upgrade/templates/src/main/java/org/exoplatform/ecms/upgrade/plugins/NodePathUpgradePlugin.java
@@ -5,7 +5,6 @@ import javax.jcr.Session;
 import javax.jcr.NodeIterator;
 
 import org.exoplatform.commons.upgrade.UpgradeProductPlugin;
-import org.exoplatform.commons.utils.PrivilegedSystemHelper;
 import org.exoplatform.container.xml.InitParams;
 import org.exoplatform.services.jcr.RepositoryService;
 import org.exoplatform.services.jcr.ext.common.SessionProvider;
@@ -26,17 +25,19 @@ public class NodePathUpgradePlugin extends UpgradeProductPlugin {
   
   private Log log = ExoLogger.getLogger(this.getClass());
   private RepositoryService repositoryService_;
+  private String srcNode;
+  private String destNode;
   
   public NodePathUpgradePlugin(RepositoryService repoService, InitParams initParams){
     super(initParams);
     this.repositoryService_ = repoService;
+    this.srcNode= initParams.getValueParam("source.node").getValue();
+    this.destNode = initParams.getValueParam("target.node").getValue();
   }
   
   @Override
   public void processUpgrade(String oldVersion, String newVersion) {
     log.info("Start " + this.getClass().getName() + ".............");
-    String srcNode = PrivilegedSystemHelper.getProperty("source-node");
-    String destNode = PrivilegedSystemHelper.getProperty("target-node");
     if ((srcNode == null) || ("".equals(srcNode)) || (destNode == null) || ("".equals(destNode))) {
       log.info("Source and target node must be set to run plugin!");
       return;
@@ -47,23 +48,22 @@ public class NodePathUpgradePlugin extends UpgradeProductPlugin {
     try{
       Session session = sessionProvider.getSession(workspace, repositoryService_.getCurrentRepository());
       Node sourceNode = session.getRootNode().getNode(nodePath.substring(1));
+      Node targetNode = session.getRootNode().getNode(destNode.substring(1));
       if (sourceNode.hasNodes()){
         NodeIterator iter = sourceNode.getNodes();
         while (iter.hasNext()){
           Node child = (Node) iter.next();
+          if (targetNode.hasNode(child.getName())) {
+        	targetNode.getNode(child.getName()).remove();
+        	session.save();
+          }
           log.info("Move " + nodePath + "/" + child.getName() + " to " + destNode + "/" + child.getName());
           session.move(nodePath + "/" + child.getName(), destNode + "/" + child.getName());
           session.save();
         }
         //Remove source node
-        if (nodePath.substring(1).contains("/")){
-          Node parentNode = session.getRootNode().getNode(nodePath.substring(1).split("/")[0]);
-          parentNode.remove();
-          session.save();
-        } else {
-          sourceNode.remove();
-          session.save();
-        }
+        sourceNode.remove();
+        session.save();
       }
     }catch(Exception e){
       log.error("Unexpected error happens in moving nodes", e.getMessage());
