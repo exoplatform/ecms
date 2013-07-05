4a00c55d9c3d55f5c3a1ef8c325a49a18083bd2a
luongtrunghieu <hieult2@exoplatform.com>
2013-07-05 16:45:36 +0700

    ECMS-5159 Incorrect UI of Add Category Tree

--- core/webui/src/main/java/org/exoplatform/ecm/webui/form/UIDialogForm.java
+++ core/webui/src/main/java/org/exoplatform/ecm/webui/form/UIDialogForm.java
@@ -1682,7 +1682,7 @@ public class UIDialogForm extends UIForm {
               + "','RemoveReference','&objectId="
               + fieldName
               + "' )\"><i"
-              +" class='uiIconTrash'></i>"
+              +" class='uiIconTrash uiIconLightGray'></i>"
               + "</a>");
       }
     }
