50da9b35e6fe4b1168a273368d647c7483dc5d7e
tungnq <tungnq@exoplatform.com>
2013-07-19 13:51:50 +0700

    ECMS-5103: Not show correctly category with special characters

--- core/webui-administration/src/main/java/org/exoplatform/ecm/webui/component/admin/taxonomy/UITaxonomyTreeCreateChildForm.java
+++ core/webui-administration/src/main/java/org/exoplatform/ecm/webui/component/admin/taxonomy/UITaxonomyTreeCreateChildForm.java
@@ -17,14 +17,18 @@
  **************************************************************************/
 package org.exoplatform.ecm.webui.component.admin.taxonomy;
 
+import javax.jcr.Node;
 import javax.jcr.RepositoryException;
+import javax.jcr.Session;
 
-import org.exoplatform.ecm.webui.form.validator.ECMNameValidator;
-import org.exoplatform.ecm.webui.utils.Utils;
 import org.exoplatform.portal.webui.util.Util;
 import org.exoplatform.services.cms.taxonomy.TaxonomyService;
 import org.exoplatform.services.cms.taxonomy.TaxonomyTreeData;
 import org.exoplatform.services.cms.taxonomy.impl.TaxonomyNodeAlreadyExistsException;
+import org.exoplatform.services.jcr.ext.common.SessionProvider;
+import org.exoplatform.services.jcr.util.Text;
+import org.exoplatform.services.wcm.utils.WCMCoreUtils;
+import org.exoplatform.wcm.webui.Utils;
 import org.exoplatform.web.application.ApplicationMessage;
 import org.exoplatform.webui.config.annotation.ComponentConfig;
 import org.exoplatform.webui.config.annotation.EventConfig;
@@ -32,8 +36,8 @@ import org.exoplatform.webui.core.UIApplication;
 import org.exoplatform.webui.core.UIPopupWindow;
 import org.exoplatform.webui.core.lifecycle.UIFormLifecycle;
 import org.exoplatform.webui.event.Event;
-import org.exoplatform.webui.event.EventListener;
 import org.exoplatform.webui.event.Event.Phase;
+import org.exoplatform.webui.event.EventListener;
 import org.exoplatform.webui.form.UIForm;
 import org.exoplatform.webui.form.UIFormInputInfo;
 import org.exoplatform.webui.form.UIFormStringInput;
@@ -63,8 +67,7 @@ public class UITaxonomyTreeCreateChildForm extends UIForm {
 
   public UITaxonomyTreeCreateChildForm() throws Exception {
     addUIFormInput(new UIFormInputInfo(FIELD_PARENT, FIELD_PARENT, null));
-    addUIFormInput(new UIFormStringInput(FIELD_NAME, FIELD_NAME, null).addValidator(
-        MandatoryValidator.class).addValidator(ECMNameValidator.class));
+    addUIFormInput(new UIFormStringInput(FIELD_NAME, FIELD_NAME, null).addValidator(MandatoryValidator.class));
   }
 
   public void setParent(String path) {
@@ -80,26 +83,20 @@ public class UITaxonomyTreeCreateChildForm extends UIForm {
       UITaxonomyManagerTrees uiTaxonomyManageTrees = uiForm.getAncestorOfType(UITaxonomyManagerTrees.class);
       TaxonomyTreeData taxoTreeData = uiTaxonomyTreeContainer.getTaxonomyTreeData();
       UIApplication uiApp = uiForm.getAncestorOfType(UIApplication.class);
-      String name = uiForm.getUIStringInput(FIELD_NAME).getValue();
-      if (name == null || name.trim().length() == 0) {
+      String title = uiForm.getUIStringInput(FIELD_NAME).getValue();
+      
+      String name = Utils.cleanString(title);
+      
+      if (name == null || name.length() == 0) {
         uiApp.addMessage(new ApplicationMessage("UITaxonomyTreeCreateChildForm.msg.name-null",
             null, ApplicationMessage.WARNING));
         
         return;
       }
-      else
-        name = name.trim();
 
-      if (!Utils.isNameValid(name, new String[] { "&", "$", "@", ",", ":", "]", "[", "*", "%", "!" })) {
-        uiApp.addMessage(new ApplicationMessage("UITaxonomyTreeCreateChildForm.msg.name-invalid",
-            null, ApplicationMessage.WARNING));
-        
-        return;
-      }
-      
       try {
         TaxonomyService taxonomyService = uiForm.getApplicationComponent(TaxonomyService.class);
-        if (name.length() > Integer.parseInt(taxonomyService.getCategoryNameLength())) {
+        if (title.length() > Integer.parseInt(taxonomyService.getCategoryNameLength())) {
           Object[] args = { taxonomyService.getCategoryNameLength() };
           uiApp.addMessage(new ApplicationMessage("UITaxonomyTreeCreateChildForm.msg.name-too-long",
                                                   args,
@@ -109,7 +106,18 @@ public class UITaxonomyTreeCreateChildForm extends UIForm {
         }
         String parentPath = uiForm.getUIFormInputInfo(FIELD_PARENT).getValue();
         taxonomyService.addTaxonomyNode(taxoTreeData
-            .getTaxoTreeWorkspace(), parentPath, name, Util.getPortalRequestContext().getRemoteUser());
+            .getTaxoTreeWorkspace(), parentPath, Text.escapeIllegalJcrChars(name), Util.getPortalRequestContext().getRemoteUser());
+        
+        SessionProvider p = WCMCoreUtils.getUserSessionProvider();
+        Session session = p.getSession(taxoTreeData.getTaxoTreeWorkspace(), WCMCoreUtils.getRepository());
+        Node newNode = (Node)session.getItem(parentPath + "/" + name); 
+        
+        if (newNode.canAddMixin("exo:rss-enable")) {
+          newNode.addMixin("exo:rss-enable");
+          newNode.setProperty("exo:title", title);
+        }
+        newNode.save();
+        
         uiCreateChild.update();
       } catch (TaxonomyNodeAlreadyExistsException e) {
         Object[] arg = { name };
