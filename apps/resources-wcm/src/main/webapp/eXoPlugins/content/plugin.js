cadf946d24d1d5c491ff70a8c7742f05aedabda6
Pham Duy Dong <dongpd@exoplatform.com>
2013-07-24 12:09:55 +0700

    ECMS-5452: CLONE - FCK editor does not load after opening insert image ("image icon") with IE
    Fix description:
      - Use flag to mark if the Content Selector opened. When this flag is on, do not update Ajax.

--- apps/resources-wcm/src/main/webapp/eXoPlugins/content/plugin.js
+++ apps/resources-wcm/src/main/webapp/eXoPlugins/content/plugin.js
@@ -22,4 +22,5 @@ CKEDITOR.plugins.add('content',
 
 function showContentSelector(e){
 	window.open(CKEDITOR.eXoPath+'eXoPlugins/content/content.html?insertContentType=All&viewType=list&currentInstance='+e.name,'WCMGadgetSelector','width=1024,height=700');
+	window.popup_opened = true;
 }
