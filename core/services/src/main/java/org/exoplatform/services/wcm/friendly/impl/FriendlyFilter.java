2afd01381d3c5f44c0b58bc7767cca16934b13ca
anhvurz90 <anhvurz90@gmail.com>
2013-10-15 13:47:12 +0700

    ECMS-5664 fix the case for rest url

--- core/services/src/main/java/org/exoplatform/services/wcm/friendly/impl/FriendlyFilter.java
+++ core/services/src/main/java/org/exoplatform/services/wcm/friendly/impl/FriendlyFilter.java
@@ -17,23 +17,17 @@
 package org.exoplatform.services.wcm.friendly.impl;
 
 import java.io.IOException;
-import java.util.Enumeration;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletRequestWrapper;
 
+import org.exoplatform.container.definition.PortalContainerConfig;
 import org.exoplatform.services.wcm.friendly.FriendlyService;
 import org.exoplatform.services.wcm.utils.WCMCoreUtils;
 import org.exoplatform.web.filter.Filter;
-import org.gatein.common.http.QueryStringParser;
-import org.gatein.common.util.ParameterMap;
 
 /**
  * Created by The eXo Platform SAS
@@ -45,123 +39,25 @@ import org.gatein.common.util.ParameterMap;
  */
 public class FriendlyFilter implements Filter {
 
-  
   @Override
   public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException,
                                                                                            ServletException {
     FriendlyService friendlyService = WCMCoreUtils.getService(FriendlyService.class);
     if (request instanceof HttpServletRequest) {
       String uri = ((HttpServletRequest)request).getRequestURI();
-      String friendlyUri = friendlyService.getUnfriendlyUri(uri);
-      if (uri != null && !uri.equals(friendlyUri)) {
-        chain.doFilter(new FriendlyServletRequestWrapper((HttpServletRequest)request), 
-                       response);
+      String unfriendlyUri = friendlyService.getUnfriendlyUri(uri);
+      if (uri != null && !uri.equals(unfriendlyUri)) {
+        String portalName = "/" + WCMCoreUtils.getPortalName();
+        if (unfriendlyUri.startsWith(portalName)) {
+          unfriendlyUri = unfriendlyUri.substring(portalName.length());
+        }
+        ((HttpServletRequest)request).getRequestDispatcher(unfriendlyUri).forward(request, response);
       } else {
         chain.doFilter(request, response);
       }
     } else {
       chain.doFilter(request, response);
     }
-  }
-  
-  /**
-   * Wraps servlet request and change friendly url to unfriendy url.
-   * @author vuna
-   *
-   */
-  public class FriendlyServletRequestWrapper extends HttpServletRequestWrapper {
-
-    private ParameterMap newParamMap_;
-    private String uri_ = "";
-    private String url_ = "";
-    private String queryString_ = "";
-    
-    public FriendlyServletRequestWrapper(HttpServletRequest request) {
-      super(request);
-      FriendlyService friendlyService = WCMCoreUtils.getService(FriendlyService.class);
-      uri_ = friendlyService.getUnfriendlyUri(request.getRequestURI());
-      url_ = friendlyService.getUnfriendlyUri(request.getRequestURL().toString());
-      int id = uri_.indexOf("?");
-      if (id > -1) {
-        queryString_ = uri_.substring(id + 1);
-        uri_ = uri_.substring(0, id);
-      }
-      id = url_.indexOf("?");
-      if (id > -1) {
-        url_ = url_.substring(0, id);
-      }
-      
-      newParamMap_ = QueryStringParser.getInstance().parseQueryString(queryString_);
-      request.getParameterNames();
-    }
-    
-    @Override
-    public String getParameter(String name) {
-      String[] rets = newParamMap_.get(name);
-      return (rets != null && rets.length > 0)? rets[0] : super.getParameter(name); 
-    }
-    
-    @Override
-    public Enumeration getParameterNames() {
-      return new FriendlyEnumeration(this.getParameterMap());
-    }
-    
-    @Override
-    public String[] getParameterValues(String name) {
-      String[] ret = newParamMap_.get(name);
-      return (ret != null)? ret : super.getParameterValues(name);
-    }
-    
-    @Override
-    public Map getParameterMap() {
-      Map oldMap = super.getParameterMap();
-      if (newParamMap_ == null) {
-        return oldMap;
-      }
-      if (oldMap == null) {
-        return newParamMap_;
-      }
-      Map ret = new HashMap();
-      ret.putAll(oldMap);
-      ret.putAll(newParamMap_);
-      return ret;
-    }
-    
-    @Override
-    public String getQueryString() {
-      return queryString_;
-    }
-    
-    @Override
-    public String getRequestURI() {
-      return uri_;
-    }
-    
-    @Override
-    public StringBuffer getRequestURL() {
-      return new StringBuffer(url_);
-    }
-    
-    public class FriendlyEnumeration implements Enumeration {
-      private Iterator iter_ = null;
-      public FriendlyEnumeration(Map paramMap) {
-        if (paramMap != null) {
-          iter_ = paramMap.keySet().iterator();
-        }
-      }
-
-      @Override
-      public boolean hasMoreElements() {
-        return (iter_ == null) ? false : iter_.hasNext();
-      }
-
-      @Override
-      public Object nextElement() {
-        return (iter_ == null) ? null : iter_.next();
-      }
-      
-    }
-    
   }
 
 }
