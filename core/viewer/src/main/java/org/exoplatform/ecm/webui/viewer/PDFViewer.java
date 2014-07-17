71f915a625d999e91cd66c6d91db90d99032d6e0
fferchichi <fferchichi@exoplatform.com>
2014-07-17 12:38:47 +0100

    remove all unnecessary imports

--- core/viewer/src/main/java/org/exoplatform/ecm/webui/viewer/PDFViewer.java
+++ core/viewer/src/main/java/org/exoplatform/ecm/webui/viewer/PDFViewer.java
@@ -23,8 +23,6 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 import java.util.ResourceBundle;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 import javax.jcr.Node;
 import javax.jcr.NodeIterator;
