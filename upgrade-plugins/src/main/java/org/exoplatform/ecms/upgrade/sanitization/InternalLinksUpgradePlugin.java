452459757c3eaa82ea2e8ac58c8a321024d3b2ba
Romain Dénarié <rdenarie@exoplatform.com>
2014-08-29 13:59:31 +0700

    ECMS-6362: InternalLinkUpgradePlugin does not upgrade all links

--- upgrade-plugins/src/main/java/org/exoplatform/ecms/upgrade/sanitization/InternalLinksUpgradePlugin.java
+++ upgrade-plugins/src/main/java/org/exoplatform/ecms/upgrade/sanitization/InternalLinksUpgradePlugin.java
@@ -61,6 +61,7 @@ public class InternalLinksUpgradePlugin extends UpgradeProductPlugin {
       documentMixinTypes.add("exo:jsFile");
       documentMixinTypes.add("exo:htmlFile");
       documentMixinTypes.add("exo:cssFile");
+      documentMixinTypes.add("exo:webContent");
       for (String type : documentMixinTypes) {
         StringBuilder statement = new StringBuilder().append("select * from ").append(type).append(" ORDER BY exo:name DESC ");
         QueryResult result = session.getWorkspace().getQueryManager().createQuery(statement.toString(), Query.SQL).execute();
@@ -68,49 +69,64 @@ public class InternalLinksUpgradePlugin extends UpgradeProductPlugin {
         while (nodeIter.hasNext()) {
           try {
             Node node = nodeIter.nextNode();
-            if (node.hasNode("jcr:content")) {
-              Node content = node.getNode("jcr:content");
-              if (content.hasProperty("jcr:mimeType")) {
-                String mimeType = content.getProperty("jcr:mimeType").getString();
-                if (mimeType.startsWith("text")) {
-                  String jcrData = content.getProperty("jcr:data").getString();
-                  if (jcrData.contains("/sites content/live/")|| jcrData.contains("/sites%20content/live/")) {
-                    LOG.info("=====Migrating data contents '"+ content.getParent().getPath()+ "' =====");
-                    String newData = StringUtils.replaceEachRepeatedly(jcrData,
-                                                                       new String[] {"/sites content/live/","/sites%20content/live/" },
-                                                                       new String[] { "/sites/","/sites/" });
-                    content.setProperty("jcr:data", newData);
-                    session.save();
-                    Node parent = node.getParent();
-                    if(parent.isNodeType("exo:webContent")) {
-                      nodesToRepublish.add(parent.getPath());
-                      continue;
+            if (type.equals("exo:webContent")) {
+                if (node.hasProperty("exo:summary")) {
+                    String summary = node.getProperty("exo:summary").getString();
+                    if (summary.contains("/sites content/live/") || summary.contains("/sites%20content/live/")) {
+                        LOG.info("=====Migrating data summary '" + node.getPath() + "' =====");
+                        String newData = StringUtils.replaceEachRepeatedly(summary,
+                                new String[]{"/sites content/live/", "/sites%20content/live/"},
+                                new String[]{"/sites/", "/sites/"});
+                        node.setProperty("exo:summary", newData);
+                        session.save();
+                        nodesToRepublish.add(node.getPath());
                     }
-                    //for subnodes in some folders like css, js, documents, medias
-                    if(parent.getPath().equals("/")) {
-                      nodesToRepublish.add(node.getPath());
-                      continue;
-                    }
-                    
-                    Node grandParent = parent.getParent();
-                    if(grandParent.isNodeType("exo:webContent")) {
-                      nodesToRepublish.add(grandParent.getPath());
-                      continue;
-                    }
-                    //for subnodes in some folders like images, videos, audio
-                    if(grandParent.getPath().equals("/")){
-                      nodesToRepublish.add(node.getPath());
-                      continue;
-                    }
-                    Node ancestor = grandParent.getParent();
-                    if(ancestor.isNodeType("exo:webContent")) {
-                      nodesToRepublish.add(ancestor.getPath());
-                      continue;
-                    }
-                    nodesToRepublish.add(node.getPath());
-                  }
                 }
-              }
+            } else {
+                if (node.hasNode("jcr:content")) {
+                    Node content = node.getNode("jcr:content");
+                    if (content.hasProperty("jcr:mimeType")) {
+                        String mimeType = content.getProperty("jcr:mimeType").getString();
+                        if (mimeType.startsWith("text") || (type.equals("exo:jsFile") && mimeType.equals("application/x-javascript"))) {
+                            String jcrData = content.getProperty("jcr:data").getString();
+                            if (jcrData.contains("/sites content/live/") || jcrData.contains("/sites%20content/live/")) {
+                                LOG.info("=====Migrating data contents '" + content.getParent().getPath() + "' =====");
+                                String newData = StringUtils.replaceEachRepeatedly(jcrData,
+                                        new String[]{"/sites content/live/", "/sites%20content/live/"},
+                                        new String[]{"/sites/", "/sites/"});
+                                content.setProperty("jcr:data", newData);
+                                session.save();
+                                Node parent = node.getParent();
+                                if (parent.isNodeType("exo:webContent")) {
+                                    nodesToRepublish.add(parent.getPath());
+                                    continue;
+                                }
+                                //for subnodes in some folders like css, js, documents, medias
+                                if (parent.getPath().equals("/")) {
+                                    nodesToRepublish.add(node.getPath());
+                                    continue;
+                                }
+
+                                Node grandParent = parent.getParent();
+                                if (grandParent.isNodeType("exo:webContent")) {
+                                    nodesToRepublish.add(grandParent.getPath());
+                                    continue;
+                                }
+                                //for subnodes in some folders like images, videos, audio
+                                if (grandParent.getPath().equals("/")) {
+                                    nodesToRepublish.add(node.getPath());
+                                    continue;
+                                }
+                                Node ancestor = grandParent.getParent();
+                                if (ancestor.isNodeType("exo:webContent")) {
+                                    nodesToRepublish.add(ancestor.getPath());
+                                    continue;
+                                }
+                                nodesToRepublish.add(node.getPath());
+                            }
+                        }
+                    }
+                }
             }
           } catch (Exception e) {
             LOG.error("An unexpected error occurs when migrating JCR Data Content: ",e);
