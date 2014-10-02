24f95252184c0d43d55613796fea453c80df46e5
anhvurz90 <vuna@exoplatform.com>
2014-10-02 11:46:26 +0700

    ECMS-6420 fix translation configuration for vi and fr in acceptInline and cancelInline plugins

--- apps/resources-wcm/src/main/webapp/eXoPlugins/acceptInline/lang/fr.js
+++ apps/resources-wcm/src/main/webapp/eXoPlugins/acceptInline/lang/fr.js
@@ -1,44 +1,4 @@
-var UserLanguage = {
-	Filter							:	"Filtre",
-	All									: "tout",
-	WebContents						:   "Contenu Web",
-	DMSDocuments					:	"DMS Document",
-	Medias							:	"Medias",
-	UploadFile						:	"Envoyer un fichier",
-
-	SettingTitle					: 	"Paramètre",
-	SetView							: 	"Affichage",
-	SetSort							: 	"Trier",
-	
-	SortName						:	"Nom du fichier",
-	SortDate						: 	"Date",
-	SortSize						: 	"Taille",
-	
-	ViewThumbnail					: 	"Thumbnail",
-	ViewList						: 	"Liste",
-	
-	Home							:	"Accueil",
-
-	FileName						:	"Nom",
-	CreateDate						:	"Date",
-	FileSize						:	"Taille",	
-
-	NoContent						:	"Il n'ya pas de contenu",
-
-	BtnCancel						: 	"Annuler",
-	BtnUpload						: 	"Envoyer",
-	BtnSetting						: 	"Paramètre",
-	BtnClose						: 	"Fermer",
-	BtnSave							: 	"Sauver",
-	BtnAbort						: 	"Abort",
-	BtnDelete						: 	"Supprimer",
-	
-	LabelFolder						: 	"Dossier",
-	LabelAlert						: 	"Veuillez choisir un fichier avant l'upload",
-	LabelName						: 	"Nom"
-}
-
-CKEDITOR.plugins.setLang( 'content', 'fr',
+CKEDITOR.plugins.setLang( 'acceptInline', 'fr',
 { 
-  WCMInsertContentPlugins        : 'Insérer un lien contenu'
+  AcceptUpdateInline        : 'Accepter'
 } );
