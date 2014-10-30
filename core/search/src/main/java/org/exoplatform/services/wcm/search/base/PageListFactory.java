eea3dcafd6e1127447de6f4c4960cd2106e5a24b
rdenarie <romain.denarie@exoplatform.com>
2014-10-30 14:52:43 +0100

    ECMS-6444 : remove a warning in console

--- core/search/src/main/java/org/exoplatform/services/wcm/search/base/PageListFactory.java
+++ core/search/src/main/java/org/exoplatform/services/wcm/search/base/PageListFactory.java
@@ -76,6 +76,7 @@ public class PageListFactory {
     ((QueryImpl)query).setLimit(AbstractPageList.RESULT_SIZE_SEPARATOR + 1);
     QueryResult result = query.execute();
     int totalNodes = (int)result.getNodes().getSize();
+    totalNodes = totalNodes < 0 ? 0 : totalNodes;
     if (totalNodes <= AbstractPageList.RESULT_SIZE_SEPARATOR) {
       return new ArrayNodePageList<E>(result, pageSize, filter, dataCreator);
     } else {
