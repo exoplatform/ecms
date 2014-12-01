fbca02de3f7877e00aaf976b0d795e51067b75e8
Pham Duy Dong <dongpd@exoplatform.com>
2014-12-01 17:37:44 +0700

    Fix popup screen

--- apps/resources-wcm/src/main/webapp/javascript/eXo/wcm/backoffice/private/DocumentPreview.js
+++ apps/resources-wcm/src/main/webapp/javascript/eXo/wcm/backoffice/private/DocumentPreview.js
@@ -2,18 +2,27 @@
   function DocumentPreview() {
   }
 
-  eXo.ecm.DocumentPreview = new DocumentPreview();
-  DocumentPreview.prototype.bindClose = function() {
+  DocumentPreview.prototype.initScreen = function () {
+
+    // Temporarily disable body scroll
+    gj('body').css('overflow', 'hidden');
+
     // Bind Esc key
-    gj('#UIDocumentPreview').keyup(function(e) {
-      if(e.keyCode == 27) {
-        alert("xxx");
+    gj('#UIDocumentPreview').keyup(function (e) {
+      if (e.keyCode == 27) {
         gj(".uiIconClose", this).trigger("click");
       }
     });
+
+    // Return body scroll
+    gj(".uiIconClose", document.getElementById('UIDocumentPreview')).click(function () {
+      gj('body').css('overflow', 'visible');
+    });
   };
 
+  eXo.ecm.DocumentPreview = new DocumentPreview();
+
   return {
-    DocumentPreview : eXo.ecm.DocumentPreview
+    DocumentPreview: eXo.ecm.DocumentPreview
   };
 })(gj, base);
