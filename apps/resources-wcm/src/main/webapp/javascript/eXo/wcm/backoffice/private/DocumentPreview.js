975639a5c38b5c2d0d65b6f6e9d1b4551adf91d0
Pham Duy Dong <dongpd@exoplatform.com>
2014-12-17 17:21:58 +0700

    Add/Remove comment in Comment Area

--- apps/resources-wcm/src/main/webapp/javascript/eXo/wcm/backoffice/private/DocumentPreview.js
+++ apps/resources-wcm/src/main/webapp/javascript/eXo/wcm/backoffice/private/DocumentPreview.js
@@ -1,5 +1,11 @@
 (function(gj, base) {
-    function DocumentPreview() {}
+    function DocumentPreview() {
+        this.refreshCommentsAction = "";
+    }
+
+    DocumentPreview.prototype.setRefreshCommentsAction = function(refreshCommentsAction) {
+        this.refreshCommentsAction = refreshCommentsAction;
+    };
 
     DocumentPreview.prototype.initScreen = function() {
 
@@ -11,7 +17,7 @@
         // Bind Esc key
         var closeEventHandler = function(e) {
             if (e.keyCode == 27) {
-                gj(".uiIconClose", $uiDocumentPreview).trigger("click");
+                gj(".exitWindow > .uiIconClose", $uiDocumentPreview).trigger("click");
             }
         }
         gj(document).on('keyup', closeEventHandler);
@@ -23,9 +29,9 @@
             gj('#outerContainer', $uiDocumentPreview).height(pdfDisplayAreaHeight); // pdf viewer
             var $commentArea = gj('.commentArea', $uiDocumentPreview);
             var $commentAreaTitle = gj('.title', $commentArea);
-            var $commentInputBox = gj('.commentInputBox',$commentArea);
+            var $commentInputBox = gj('.commentInputBox', $commentArea);
             var $commentList = gj('.commentList', $commentArea);
-            var commentAreaHeight = window.innerHeight -30;
+            var commentAreaHeight = window.innerHeight - 30;
             $commentArea.height(commentAreaHeight);
             $commentList.css('max-height', commentAreaHeight - $commentAreaTitle.innerHeight() - $commentInputBox.innerHeight());
             $commentList.scrollTop(20000);
@@ -44,8 +50,8 @@
             $navigationContainer.height(pdfDisplayAreaHeight);
             $uiContentBox.height(pdfDisplayAreaHeight);
             $flowplayerContentDetail.height(pdfDisplayAreaHeight);
-            $flowplayerPlayerContent.height(pdfDisplayAreaHeight-5);
-            $flashViewer.height(pdfDisplayAreaHeight-5);
+            $flowplayerPlayerContent.height(pdfDisplayAreaHeight - 5);
+            $flashViewer.height(pdfDisplayAreaHeight - 5);
 
             $flowplayer.css('max-width', $uiContentBox.width() - 2);
             $flowplayer.css('max-height', $uiContentBox.height() - 3);
@@ -79,7 +85,7 @@
         gj(window).on('resize', resizeEventHandler);
 
         // Bind close event. Return body scroll, turn off keyup
-        gj(".uiIconClose", $uiDocumentPreview).click(function() {
+        gj(".exitWindow > .uiIconClose", $uiDocumentPreview).click(function() {
             setTimeout(function() {
                 gj('body').css('overflow', 'visible');
                 gj(document).off('keyup', closeEventHandler);
@@ -96,18 +102,47 @@
             var $resizeButton = gj('.resizeButton', $uiDocumentPreview);
             if ($uiIconMiniArrow.hasClass('uiIconMiniArrowRight')) {
                 $commentArea.css('display', 'none');
-                $uiPreviewWebContent.css('margin-right','30px');
+                $uiPreviewWebContent.css('margin-right', '30px');
                 $fileContent.css('margin-right', '30px');
                 $resizeButton.css('right', '5px');
             } else {
                 $commentArea.css('display', 'block');
-                $uiPreviewWebContent.css('margin-right','335px');
+                $uiPreviewWebContent.css('margin-right', '335px');
                 $fileContent.css('margin-right', '335px');
                 $resizeButton.css('right', '310px');
             }
             $uiIconMiniArrow.toggleClass('uiIconMiniArrowLeft');
             $uiIconMiniArrow.toggleClass('uiIconMiniArrowRight');
         });
+    };
+
+    DocumentPreview.prototype.bindPostCommentEvent = function() {
+        // Bind comment text area
+        gj('#commentTextAreaPreview').unbind('keypress');
+        gj('#commentTextAreaPreview').keypress(function(e) {
+            var $commentTextAreaPreview = gj(this);
+            var code = e.keyCode || e.which;
+            if (code == 13) { // Enter keycode
+                var activityId = $commentTextAreaPreview.attr('activityId');
+                var commentText = $commentTextAreaPreview.val();
+                $commentTextAreaPreview.val('');
+                var postActivityLink = "http://localhost:8080/rest/private/portal/social/activities/" + activityId + "/comments/create.json?text=" + commentText + "&opensocial_viewer_id=" + eXo.env.portal.userName;
+
+                var jqxhr = gj.ajax(postActivityLink)
+                    .done(function() {
+                        eXo.ecm.DocumentPreview.refreshComments();
+                    })
+                    .fail(function() {
+                        alert("error");
+                    });
+
+            }
+        });
+    };
+
+    DocumentPreview.prototype.refreshComments = function() {
+      var url = decodeURIComponent(this.refreshCommentsAction);
+      eval(url);
     };
 
     eXo.ecm.DocumentPreview = new DocumentPreview();
