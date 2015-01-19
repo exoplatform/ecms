0ddf0659dbe1dbdb6b359a78f124f1fd38a76b89
trungjc <trungnguyenthanh70@gmail.com>
2015-01-19 15:29:15 +0700

    [skin-module] wrong image path

--- core/search/src/main/java/org/exoplatform/services/wcm/search/connector/BaseContentSearchServiceConnector.java
+++ core/search/src/main/java/org/exoplatform/services/wcm/search/connector/BaseContentSearchServiceConnector.java
@@ -118,7 +118,7 @@ public abstract class BaseContentSearchServiceConnector extends BaseSearchServic
    * {@inheritDoc}
    */
   protected String getImageUrl(Node node) {
-    return "/eXoSkin/skin/images/system/default/Icons/TypeIcons/uiIconsType64x64.png";
+    return "/eXoSkin/skin/images/themes/default/Icons/TypeIcons/uiIconsType64x64.png";
   }
   
   /**
