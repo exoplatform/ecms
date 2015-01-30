c24c7abcb5ba6852c58735116e44f0e3654a8f8b
anhvurz90 <vuna@exoplatform.com>
2015-01-30 10:25:10 +0700

    ECMS-6619 fix logic inside PermlinkActionComponent

--- core/webui-explorer/src/main/java/org/exoplatform/ecm/webui/component/explorer/rightclick/manager/PermlinkActionComponent.java
+++ core/webui-explorer/src/main/java/org/exoplatform/ecm/webui/component/explorer/rightclick/manager/PermlinkActionComponent.java
@@ -53,9 +53,14 @@ public class PermlinkActionComponent extends UIAbstractManagerComponent {
   public List<UIExtensionFilter> getFilters() {
     return FILTERS;
   }
-
-  public String getPermlink(Node node) throws Exception {
-    UIJCRExplorer uiExplorer = getAncestorOfType(UIJCRExplorer.class);
+  
+  public String getPermlink(UIJCRExplorer uiExplorer, Node node) throws Exception {
+    if (uiExplorer == null) {
+      uiExplorer = getAncestorOfType(UIJCRExplorer.class);
+    }
+    if (node == null) {
+      node = uiExplorer.getCurrentNode();
+    }
     PortalRequestContext pcontext = Util.getPortalRequestContext();
     String portletId = pcontext.getNodePath();
     String drivename = uiExplorer.getDriveData().getName();
@@ -70,10 +75,17 @@ public class PermlinkActionComponent extends UIAbstractManagerComponent {
     nodeURL.setQueryParameterValue("path", path);
     return nodeURL.toString();
   }
+
+  public String getPermlink(UIJCRExplorer uiExplorer) throws Exception {
+    return getPermlink(uiExplorer, null);
+  }
   
+  public String getPermlink(Node node) throws Exception {
+    return getPermlink(null, node);
+  }
+
   public String getPermlink() throws Exception {
-    UIJCRExplorer uiExplorer = getAncestorOfType(UIJCRExplorer.class);
-    return getPermlink(uiExplorer.getCurrentNode());
+    return getPermlink(null, null);
   }
 
 
