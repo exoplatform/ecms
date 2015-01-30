3f6c2eaeb92738a6cb3baf4d90225619aa0aef8d
toannh <toannh@exoplatform.com>
2015-01-30 19:14:28 +0700

    ECMS-6518 Update uisimpleview.js to correct open-doc label

--- apps/portlet-explorer/src/main/webapp/javascript/eXo/ecm/UISimpleView.js
+++ apps/portlet-explorer/src/main/webapp/javascript/eXo/ecm/UISimpleView.js
@@ -319,7 +319,7 @@
         mobileElement.appendChild(coverElement);
         document.body.appendChild(mobileElement);
       }else{
-        var _objId = element.attributes[6].value+":"+Self.srcPath;
+        var _objId = element.getAttribute("workspacename")+":"+Self.srcPath;
         eXo.ecm.OpenDocumentInOffice.updateLabel(_objId);
       }
     };
