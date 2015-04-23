d8cce49eb202ff75865813134af618d593637b9b
trungjc <trungnguyenthanh70@gmail.com>
2015-04-23 15:50:24 +0700

    UI-3770: remove missing stylesheet

--- packaging/wcm/webapp/src/main/webapp/javascript/eXo/ecm/responisve-exo.js
+++ packaging/wcm/webapp/src/main/webapp/javascript/eXo/ecm/responisve-exo.js
@@ -1,10 +1,17 @@
 require(["/eXoResources/javascript/jquery-1.7.1.js"], function(gj) {
-	$("#HomeLink").prepend("<a href='javascript:void(0)' class='mobile-visible  toggle-mobile'>&#8211;</a>");
-	$("#OfficeRight").prepend("<a href='javascript:void(0)' class='mobile-visible  toggle-rightbar'>&nbsp;</a>");
+	$("#HomeLink").prepend("<a href='javascript:void(0)' class='mobile-visible pull-left toggle-left-bar'><span>&#45;</span><span>&#45;</span><span>&#45;</span></a>");
+	$("#OfficeRight").prepend("<a href='javascript:void(0)' class='mobile-visible  toggle-right-bar'><span>&#45;</span><span>&#45;</span><span>&#45;</span></a>");
 
-	// toggle left menu
-	$('.toggle-mobile').on('click',function(){
+	// Toggle left bar
+	$('.toggle-left-bar').on('click',function(){
 		var leftNavi= $('.LeftNavigationTDContainer');
 		$('body').toggleClass('open-left-bar');		
+		$(leftNavi).toggleClass('expanded');		
+	})
+	// Toggle right bar
+	$('.toggle-right-bar').on('click',function(){
+		var leftNavi= $('.OfficeRightTDContainer ');
+		$('body').toggleClass('open-right-bar');		
+		$(leftNavi).toggleClass('expanded');		
 	})
 })
