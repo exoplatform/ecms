f4fa0e51427383ee99c34e4cf81c10e246827dec
hoafdt <hoadt@exoplatform.com>
2015-07-13 16:56:25 +0700

    UI-3774: Left navigation Responsive -js

--- packaging/wcm/webapp/src/main/webapp/javascript/eXo/ecm/responisve-exo.js
+++ packaging/wcm/webapp/src/main/webapp/javascript/eXo/ecm/responisve-exo.js
@@ -7,6 +7,7 @@ require(["/eXoResources/javascript/jquery-1.7.1.js"], function(gj) {
 		var leftNavi= $('.LeftNavigationTDContainer');
 		$('body').toggleClass('open-left-bar');		
 		$(leftNavi).toggleClass('expanded');		
+		left_nav_accordion();
 	})
 	// Toggle right bar
 	$('.toggle-right-bar').on('click',function(){
@@ -15,3 +16,33 @@ require(["/eXoResources/javascript/jquery-1.7.1.js"], function(gj) {
 		$(leftNavi).toggleClass('expanded');		
 	})
 })
+
+function left_nav_accordion(){
+	var companyNav = $('.uiCompanyNavigationPortlet .title');
+	var spaceNav = $('.uiSpaceNavigationPortlet .title');
+
+	companyNav.addClass('accordionBar clearfix');
+	companyNav.prepend('<i class="uiIconArrowDown pull-right"></i>');
+
+	spaceNav.addClass('accordionBar clearfix');
+	spaceNav.prepend('<i class="uiIconArrowRight pull-right"></i>');
+
+	$('.uiCompanyNavigations').addClass('accordionCont');
+	$('.spaceCont').addClass('accordionCont');
+	
+	var allAcc = $('#LeftNavigation .accordionCont').hide();
+	$('.uiCompanyNavigationPortlet .accordionCont').show();
+
+	$('#LeftNavigation .accordionBar').click(function(){
+		var subContent = $(this).next();
+		if (subContent.is(':visible')) {
+			return false;
+		}
+		else {
+			allAcc.prev().find('.uiIconArrowDown').attr('class','uiIconArrowRight pull-right');
+			allAcc.slideUp();
+			$(this).find('.uiIconArrowRight').attr('class','uiIconArrowDown pull-right');
+			$(this).next().slideDown();
+		}
+	});
+}
