5ca578b4ee50dfb62011b0e098871c9c238e3fac
toannh <toannh@exoplatform.com>
2015-07-15 10:27:58 +0700

    ECMS-7011 Share button is not displayed when Filename with special chars is selected.

--- apps/resources-wcm/src/main/webapp/javascript/eXo/wcm/frontoffice/public/OpenDocumentInOffice.js
+++ apps/resources-wcm/src/main/webapp/javascript/eXo/wcm/frontoffice/public/OpenDocumentInOffice.js
@@ -73,7 +73,7 @@
   OpenDocumentInOffice.prototype.updateLabel = function(objId, activityId, rightClick){
     var currentDocumentObj = {};
     gj.ajax({
-      url: restPrefix+"/office/updateDocumentTitle?objId=" + objId+"&lang="+eXo.env.portal.language,
+      url: restPrefix+"/office/updateDocumentTitle?objId=" + encodeURI(objId)+"&lang="+eXo.env.portal.language,
       dataType: "text",
       type: "GET",
       async:false
