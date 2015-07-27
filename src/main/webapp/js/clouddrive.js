20eef1e1fa07b896b3cc6bc580e50d3094d9c6fe
Peter Nedonosko <peter.nedonosko@exoplatform.com>
2015-07-28 02:40:20 +0300

    #24 Dropbox connector; adoption of general parts for embeded file view; cleanup

--- src/main/webapp/js/clouddrive.js
+++ src/main/webapp/js/clouddrive.js
@@ -16,7 +16,6 @@
 	 * Connector core class.
 	 */
 	function CloudDrive() {
-
 		
 		var prefixUrl = utils.pageBaseUrl(location);
 
@@ -910,6 +909,13 @@
 		};
 		
 		/**
+		 * Initialize text file viewer UI (based on Codemirror).
+		 */
+		this.initTextViewer = function() {
+			cloudDriveUI.initTextViewer();
+		};
+		
+		/**
 		 * Initialize context and UI.
 		 */
 		this.init = function(nodeWorkspace, nodePath) {
@@ -1552,10 +1558,6 @@
 								$(this).prepend(editIcon);
 							}
 						});
-						//$titleText.append($vswitch);
-						// for PLF 4.2
-						//$a.append($vswitch);
-						//$vswitch.addClass("pull-right");
 						$vswitch.prepend(editIcon);
 						$a.after($vswitch);
 					} else {
@@ -1571,7 +1573,7 @@
 				eXo.ecm.ECMUtils.loadContainerWidth();
 			} // else not a cloud drive or its file
 		};
-
+		
 		/**
 		 * Find link to open Personal Documents view in WCM. Can return nothing if current page doesn't
 		 * contain such element.
@@ -2160,6 +2162,90 @@
 					$permission.find("input[name='read']").prop("checked", true);
 				}
 			}
+		};
+		
+		/**
+		 * Init text file viewer (based on Codemirror).
+		 */
+		this.initTextViewer = function() {
+			// Add an action to "show plain text" tab to load the frame content to the code viewer
+			var $viewer = $("#WebContent");
+			var $code = $viewer.find("#TabCode");
+			
+			if ($viewer.size() > 0) {
+				// load Codemirror styles only if text editor tab exists 
+				// and only in window (not in iframe as gadgets may do) 
+				if (window == top) {
+					try {
+						utils.loadStyle("/cloud-drive/skin/codemirror.css");
+					} catch(e) {
+						utils.log("Error intializing text viewer.", e);
+					}
+				}
+			}
+
+			function createViewer(codeURL) {
+				var cursorCss = $viewer.css("cursor");
+				$viewer.css("cursor", "wait");
+
+				var contextFile = cloudDrive.getContextFile();
+				utils.log("contextFile: " + contextFile.path + " " + contextFile.typeMode);
+				if (!codeURL && contextFile) {
+					codeURL = contextFile.previewLink;
+				}
+
+				$.get(codeURL, function(data, status, jqXHR) {
+					try {
+						var code = jqXHR.responseText;
+						var codeMode = jqXHR.getResponseHeader("x-type-mode");
+						if (!codeMode) {
+							var contentType = jqXHR.getResponseHeader("Content-Type");
+							if (contentType) {
+								codeMode = contentType;
+							} else {
+								codeMode = "htmlmixed";
+							}
+						}
+
+						// XXX CodeMirror script already minified and cannot be loaded via PLF AMD mechanism
+						// load it be direct path from the server
+						// FYI PLF's RequireJS baseUrl is /portal/intranet, thus we need relative moduleId to reach the WAR location
+						// after all this CodeMirror will be available globally as 'require' inside the module wrapper doesn't have amd
+						// function.
+						window.require(["../../cloud-drive-cmis/js/codemirror-bundle.min"], function() {
+							CodeMirror($code.get(0), {
+								value : code,
+								lineNumbers : true,
+								readOnly : true,
+								mode : codeMode
+							});
+						});
+					} catch(e) {
+						utils.log("ERROR: CodeMirror creation error " + provider.name + "(" + provider.id + "). " + e.message + ": " + JSON.stringify(e));
+					} finally {
+						$viewer.css("cursor", cursorCss);
+					}
+				});
+			}
+
+			var $codeSwitch = $("#FileCodeSwitch");
+			$codeSwitch.click(function() {
+				if ($code.size() > 0 && $code.children().size() == 0) {
+					var codeURL = $viewer.find("iframe").attr("src");
+					createViewer(codeURL);
+				}
+			});
+
+			if ($code.is(".active")) {
+				// it is XML document, click to load its content
+				$codeSwitch.click();
+				$viewer.find("#TabHTML").css("display", "");
+			} else if ($code.is(":visible")) {
+				createViewer(null);
+			}
+
+			// XXX remove display: block, it appears for unknown reason
+			$code.css("display", "");
 		};
 		
 		/**
