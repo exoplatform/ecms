4aa8ca4a9e9f68b8d06963f01aae41fa4a936adf
Peter Nedonosko <peter.nedonosko@exoplatform.com>
2015-08-13 10:49:20 +0300

    improved default file viewer (allows only text, images and html); large files download page; cleanup in CMIS connector

--- src/main/java/org/exoplatform/clouddrive/ecms/viewer/DefaultFileViewer.java
+++ src/main/java/org/exoplatform/clouddrive/ecms/viewer/DefaultFileViewer.java
@@ -27,6 +27,8 @@ import org.exoplatform.webui.ext.filter.UIExtensionFilters;
 import java.util.Arrays;
 import java.util.List;
 
+import javax.ws.rs.core.MediaType;
+
 /**
  * Default WebUI component for Cloud Drive files. It shows content of remote file by its URL in iframe on file
  * page in eXo Documents.<br>
@@ -57,6 +59,18 @@ public class DefaultFileViewer extends AbstractFileViewer {
   @UIExtensionFilters
   public List<UIExtensionFilter> getFilters() {
     return FILTERS;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public boolean isViewable() {
+    if (super.isViewable()) {
+      String mimeType = file.getType();
+      return mimeType.startsWith("text/") || mimeType.startsWith("image/") || mimeType.startsWith("application/xhtml+");
+    }
+    return false;
   }
 
   /**
