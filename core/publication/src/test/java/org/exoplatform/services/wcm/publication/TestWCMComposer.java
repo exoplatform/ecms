ca8c1cfb695d38f69f38aae5ec0591e8edc8dbbb
Omar Bouras <omar.bouras@exoplatform.com>
2017-05-16 15:47:35 +0100

    ECMS-4084: Wrong CLV pagination with symlinks (#289)
    ECMS-4084: Wrong CLV pagination with symlinks(#289)

--- core/publication/src/test/java/org/exoplatform/services/wcm/publication/TestWCMComposer.java
+++ core/publication/src/test/java/org/exoplatform/services/wcm/publication/TestWCMComposer.java
@@ -1,9 +1,13 @@
 package org.exoplatform.services.wcm.publication;
 
+import org.exoplatform.component.test.ConfigurationUnit;
+import org.exoplatform.component.test.ConfiguredBy;
+import org.exoplatform.component.test.ContainerScope;
+import org.exoplatform.services.wcm.core.NodeLocation;
+
 import java.util.HashMap;
 
 import javax.jcr.Node;
-
 
 public class TestWCMComposer extends BasePublicationTestCase {
 
@@ -28,21 +32,47 @@ public class TestWCMComposer extends BasePublicationTestCase {
 		
 		assertNotNull(node);
 	}
-	
+
 	/**
 	 * test getContent for an non authorized node
 	 * @throws Exception
 	 */
 	public void testGetContentNotAuthorized() throws Exception {
-		
+
 		HashMap<String, String> filters = new HashMap<String, String>();
 
 		String nodeIdentifier = "/exo:application";
 		Node node = wcmComposer.getContent(COLLABORATION_WS, nodeIdentifier, filters, sessionProvider);
-		
+
 		assertNull(node);
 	}
-	
+
+	/**
+	 * test getPaginatedContents result size for an authorized node
+	 * @throws Exception
+	 */
+	public void testGetPaginatedContents() throws Exception{
+		HashMap<String, String> filters = new HashMap<String, String>();
+		String folderPath = "repository:collaboration:/sites content/live/web contents/site artifacts";
+		NodeLocation  nodeLocation = NodeLocation.getNodeLocationByExpression(folderPath);
+		//test on imported web:content
+		Result result = wcmComposer.getPaginatedContents(nodeLocation, filters, sessionProvider);
+		assertEquals(1,result.getNumTotal());
+	}
+
+	/**
+	 * test getPaginatedContents result size when FILTER_TOTAL is set
+	 * @throws Exception
+	 */
+	public void testPaginatedContentsWithFilter() throws Exception{
+		HashMap<String, String> filters = new HashMap<String, String>();
+		String folderPath = "repository:collaboration:/sites content/live";
+		NodeLocation  nodeLocation = NodeLocation.getNodeLocationByExpression(folderPath);
+		//test if FILTER_TOTAL value is already set
+		filters.put(WCMComposer.FILTER_TOTAL,"2");
+		Result result = wcmComposer.getPaginatedContents(nodeLocation, filters, sessionProvider);
+		assertEquals(2,result.getNumTotal());
+	}
 
   public void tearDown() throws Exception {
     super.tearDown();
