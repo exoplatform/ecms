fc5b4b45614d338548c847bcf77f96181d409698
AtiaZ <AtiaZ@users.noreply.github.com>
2017-05-23 16:24:16 +0100

    [ECMS-7625] Upload an image through CKeditor in the media folder of a… (#296)

--- apps/resources-wcm/src/main/webapp/eXoPlugins/content/plugin.js
+++ apps/resources-wcm/src/main/webapp/eXoPlugins/content/plugin.js
@@ -21,6 +21,21 @@ CKEDITOR.plugins.add('content',
 
 
 function showContentSelector(e){	
-	window.open('/eXoWCMResources/eXoPlugins/content/content.html?insertContentType=All&viewType=list&currentInstance='+e.name,'WCMGadgetSelector','width=1024,height=700');
+	var nodePath = null;
+	var addressBarContent = document.getElementById('AddressBarControl').children[1].defaultValue;
+	var subPath = decodeURIComponent(addressBarContent);
+	//add this condition to make the difference between the path of a WebContent which already exists and a new one
+    if ( document.getElementsByClassName('addressItem')  && document.getElementsByClassName('addressItem').length > 0) {
+    	var addressItem = document.getElementsByClassName('addressItem')["0"].childNodes["0"].data;
+        //to avoid the duplication of addressItem in case when trying to extract the nodePath of some drives (like Managed Sites) when access to them from the collaboration drive
+	   if ( ! ( addressBarContent.startsWith(addressItem) ) ){
+	       nodePath = decodeURIComponent(addressItem + addressBarContent);
+	   } else {
+	       nodePath = decodeURIComponent(addressBarContent).replace(decodeURIComponent(addressItem), "");
+	   }
+	} else {
+	   nodePath = decodeURIComponent(addressBarContent);
+	}
+	window.open('/eXoWCMResources/eXoPlugins/content/content.html?insertContentType=All&subPath='+subPath+'&nodePath='+nodePath+'&viewType=list&currentInstance='+e.name+'','WCMGadgetSelector','width=1024,height=700');
 	window.popup_opened = true;
 }
