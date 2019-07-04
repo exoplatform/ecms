e39249c432c979a4952fd4e1f9a12f5e347b210f
Igor <igorkochmarskiy@gmail.com>
2019-07-04 11:15:09 +0300

    added socketio

--- webapp/src/main/webapp/js/clouddrive-onedrive.js
+++ webapp/src/main/webapp/js/clouddrive-onedrive.js
@@ -1,12 +1,15 @@
 
 
-(function($, cloudDrive, utils) {
+(function($, cloudDrive, utils, io) {
 
     function OneDriveClient() {
+        console.log('OneDriveClient init');
+        var socket =  io("https://3-westeurope1.pushp.svc.ms/notifications?token=w1-4ef891c6-0a6a-429b-95f5-686c3e29427c");
 
-        this.initFile = function (file) {
 
-        };
+        socket.on("connect", ()=>console.log("EEEE.......Connected!"));
+        socket.on("notification", (data)=>console.log("EEE.....Notification!", data));
+
 
         this.initContext = function (provider) {
             if ('onedrive' == provider.id.trim().toLowerCase()) {
@@ -38,9 +41,34 @@
 
             }
         };
+
+
+        var getChange = function (drive) {
+            // must be blocking
+        };
+
+        //
+        // this.onChange = function(drive) {
+        //     var process = $.Deferred();
+        //
+        //     if (drive) {
+        //         // utils.log(">>> enabling changes monitor for Cloud Drive " + drive.path);
+        //         if (drive.state) {
+        //             // Drive supports state - thus we can send connector specific data via it from Java API
+        //             // State it is a POJO in JavaAPI. Here it is a JSON object.
+        //             pollChanges(process, drive);
+        //         } else {
+        //             process.reject("Cannot check for changes. No state object for Cloud Drive on " + drive.path);
+        //         }
+        //     } else {
+        //         process.reject("Null drive in onChange()");
+        //     }
+        //     return process.promise();
+        // };
+
     }
     return new OneDriveClient();
 
 
 
-})($, cloudDrive, cloudDriveUtils);
+})($, cloudDrive, cloudDriveUtils, socketIO);
