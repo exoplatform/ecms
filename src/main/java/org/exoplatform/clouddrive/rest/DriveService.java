9646d1d2fbf139f391cdc7e35bfb6cf8def4302e
Peter Nedonosko <pnedonosko@exoplatform.com>
2019-08-27 00:29:58 +0300

    Improve file initModified for cases when node session expired or closed

--- src/main/java/org/exoplatform/clouddrive/rest/DriveService.java
+++ src/main/java/org/exoplatform/clouddrive/rest/DriveService.java
@@ -181,7 +181,7 @@ public class DriveService implements ResourceContainer {
             Command sync = local.synchronize();
             sync.await(); // wait for sync process
             files = sync.getFiles();
-            initModified(files, locale);
+            initModified(files, locale, local);
             removed = sync.getRemoved();
             messages = sync.getMessages();
           } catch (InterruptedException e) {
@@ -251,7 +251,7 @@ public class DriveService implements ResourceContainer {
                 // it's symlink, add it also
                 files.add(new LinkedCloudFile(file, path));
               }
-              initModified(file, locale);
+              initModified(file, locale, local);
             }
           } catch (NotYetCloudFileException e) {
             if (LOG.isDebugEnabled()) {
@@ -323,7 +323,7 @@ public class DriveService implements ResourceContainer {
               if (!file.getPath().equals(path)) {
                 file = new LinkedCloudFile(file, path); // it's symlink
               }
-              initModified(file, locale);
+              initModified(file, locale, local);
               return Response.ok().entity(file).build();
             } catch (NotYetCloudFileException e) {
               return Response.status(Status.ACCEPTED).entity(new AcceptedCloudFile(path)).build();
@@ -413,7 +413,7 @@ public class DriveService implements ResourceContainer {
                   if (!file.getPath().equals(filePath)) {
                     file = new LinkedCloudFile(file, filePath); // it's symlink
                   }
-                  initModified(file, locale);
+                  initModified(file, locale, local);
                   files.add(file);
                 } // not a cloud file - skip it
               } catch (NotYetCloudFileException e) {
@@ -512,16 +512,16 @@ public class DriveService implements ResourceContainer {
     }
   }
 
-  private void initModified(Collection<CloudFile> files, Locale locale) {
+  private void initModified(Collection<CloudFile> files, Locale locale, CloudDrive drive) {
     for (CloudFile file : files) {
-      initModified(file, locale);
+      initModified(file, locale, drive);
     }
   }
 
-  private void initModified(CloudFile file, Locale locale) {
+  private void initModified(CloudFile file, Locale locale, CloudDrive drive) {
     if (file.isConnected()) {
       try {
-        LocalCloudFile.class.cast(file).initModified(locale);
+        LocalCloudFile.class.cast(file).initModified(locale, drive);
       } catch (ClassCastException e) {
         // safely ignore it, but let to know it was
         LOG.warn("Cannot initialize cloud file modified field for {} due to error: {}", file.getPath(), e.getMessage());
