d7a3ed326eb9bb53afe12009cd96ecf0cb861883
Boubaker Khanfir <bkhanfir@exoplatform.com>
2020-01-08 07:17:13 +0100

    Delete unused methods

--- core/search/src/main/java/org/exoplatform/services/wcm/search/listener/SeoESListenerImpl.java
+++ core/search/src/main/java/org/exoplatform/services/wcm/search/listener/SeoESListenerImpl.java
@@ -1,30 +1,18 @@
 package org.exoplatform.services.wcm.search.listener;
 
-import java.util.*;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.ArrayList;
+import java.util.List;
 
-import org.apache.commons.lang.StringUtils;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
-
-import org.exoplatform.commons.search.es.ElasticSearchException;
 import org.exoplatform.commons.search.index.IndexingService;
 import org.exoplatform.portal.mop.navigation.NavigationStore;
 import org.exoplatform.portal.mop.navigation.NodeData;
 import org.exoplatform.services.listener.Event;
 import org.exoplatform.services.listener.Listener;
-import org.exoplatform.services.log.ExoLogger;
-import org.exoplatform.services.log.Log;
 import org.exoplatform.services.seo.PageMetadataModel;
 import org.exoplatform.services.seo.SEOService;
 import org.exoplatform.services.wcm.search.connector.NavigationIndexingServiceConnector;
 
 public class SeoESListenerImpl extends Listener<SEOService, PageMetadataModel> {
-
-  private static final Log LOG = ExoLogger.getExoLogger(SeoESListenerImpl.class);
 
   private IndexingService indexingService;
 
@@ -56,116 +44,6 @@ public class SeoESListenerImpl extends Listener<SEOService, PageMetadataModel> {
       ids.add(node.getId());
     }
     return ids;
-  }
-
-  protected String buildFilteredQuery(String query, Collection<String> sites) {
-    String escapedQuery = escapeReservedCharacters(query);
-
-    StringBuilder esQuery = new StringBuilder();
-    esQuery.append("{\n");
-
-    //Score are always tracked, even with sort
-    //https://www.impl.co/guide/en/elasticsearch/reference/current/search-request-sort.html#_track_scores
-    esQuery.append("     \"track_scores\": true,\n");
-    esQuery.append("     \"_source\": [\"nodeId\"],");
-    esQuery.append("     \"query\": {\n");
-    esQuery.append("        \"bool\" : {\n");
-    esQuery.append("            \"must\" : {\n");
-    esQuery.append("                \"query_string\" : {\n");
-    esQuery.append("                    \"fields\" : [\"nodeId\"],\n");
-    esQuery.append("                    \"query\" : \"" + escapedQuery + "\"\n");
-    esQuery.append("                }\n");
-    esQuery.append("            },\n");
-    esQuery.append("            \"filter\" : {\n");
-    esQuery.append("              \"bool\" : {\n");
-    esQuery.append("                \"must\" : [\n");
-    String sitesFilter = getSitesFilter(sites);
-    if (StringUtils.isNotBlank(sitesFilter)) {
-      esQuery.append("                  {\n");
-      esQuery.append("                   \"bool\" : {\n");
-      esQuery.append("                     \"should\" : \n");
-      esQuery.append("                      " + sitesFilter + "\n");
-      esQuery.append("                    }\n");
-      esQuery.append("                  }");
-    }
-    esQuery.append("                  \n");
-    esQuery.append("                ]\n");
-    esQuery.append("              }\n");
-    esQuery.append("            }");
-    esQuery.append("        }\n");
-    esQuery.append("     }\n");
-    esQuery.append("}");
-
-    LOG.debug("Search Query request to ES : {} ", esQuery);
-
-    return esQuery.toString();
-  }
-
-  protected String getSitesFilter(Collection<String> sitesCollection) {
-    if ((sitesCollection != null) && (sitesCollection.size() > 0)) {
-      List<String> sites = new ArrayList<>();
-      for (String site : sitesCollection) {
-        sites.add("\"" + site + "\"");
-      }
-      String sitesList = "[" + StringUtils.join(sites, ",") + "]";
-      return " [ { \"bool\" : {\n" +
-              "         \"must_not\": {\n" +
-              "           \"exists\" : { \"field\" : \"sites\" }\n" +
-              "         }\n" +
-              "       }\n" +
-              "},\n" +
-              "{\n" +
-              "  \"terms\" : { \n" +
-              "    \"sites\" : " + sitesList + "\n" +
-              "  }\n" +
-              "} ]";
-    } else {
-      return " { \"bool\" : " +
-              "{\n" +
-              "  \"must_not\": {\n" +
-              "      \"exists\" : { \"field\" : \"sites\" }\n" +
-              "   }\n" +
-              "  }\n" +
-              "}\n";
-    }
-  }
-
-  protected String escapeReservedCharacters(String query) {
-    if (StringUtils.isNotEmpty(query)) {
-      return query.replaceAll("[" + Pattern.quote("+-=&|><!(){}\\[\\]^\"*?:\\/") + "]",
-              Matcher.quoteReplacement("\\\\") + "$0");
-    } else {
-      return query;
-    }
-  }
-
-  protected List<String> buildResult(String jsonResponse) {
-
-    LOG.debug("Search Query response from ES : {} ", jsonResponse);
-
-    List<String> results = new ArrayList<>();
-    JSONParser parser = new JSONParser();
-
-    Map json;
-    try {
-      json = (Map) parser.parse(jsonResponse);
-    } catch (ParseException e) {
-      throw new ElasticSearchException("Unable to parse JSON response", e);
-    }
-
-    JSONObject jsonResult = (JSONObject) json.get("hits");
-    if (jsonResult != null) {
-      JSONArray jsonHits = (JSONArray) jsonResult.get("hits");
-
-      if (jsonHits != null) {
-        for (Object jsonHit : jsonHits) {
-          results.add((String) ((JSONObject) jsonHit).get("_id"));
-        }
-      }
-    }
-
-    return results;
-
   }
 
 }
