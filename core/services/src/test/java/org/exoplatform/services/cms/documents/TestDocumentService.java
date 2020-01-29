06203a2024a16978058da618d1f0d7e265b1b847
VladMartyshevskyi <vladmartishevskii@gmail.com>
2020-01-29 16:27:55 +0200

    Added tests

---
+++ core/services/src/test/java/org/exoplatform/services/cms/documents/TestDocumentService.java
@@ -0,0 +1,47 @@
+package org.exoplatform.services.cms.documents;
+
+import javax.jcr.Node;
+
+import org.exoplatform.services.wcm.BaseWCMTestCase;
+
+public class TestDocumentService extends BaseWCMTestCase {
+
+  private DocumentService documentService;
+
+  public void setUp() throws Exception {
+    super.setUp();
+    documentService = container.getComponentInstanceOfType(DocumentService.class);
+    applySystemSession();
+  }
+
+  public void testCreateDocumentFromEmptyTemplate() throws Exception {
+    String mime = "application/vnd.openxmlformats-officedocument.wordprocessingml.document";
+    String title = "Test.docx";
+    Node parent = session.getRootNode();
+    DocumentTemplate template = new DocumentTemplate();
+    template.setMimeType(mime);
+    Node document = documentService.createDocumentFromTemplate(parent, title, template);
+    assertNotNull("Document wasn't created", document);
+    assertEquals("Title is not correct", title, document.getProperty("exo:title").getString());
+    assertTrue("Content node wasn't created", document.hasNode("jcr:content"));
+    Node content = document.getNode("jcr:content");
+    assertEquals("Mime-type is not correct", mime, content.getProperty("jcr:mimeType").getString());
+    assertEquals("Mime-type is not correct", "", content.getProperty("jcr:data").getString());
+  }
+  
+  public void tearDown() throws Exception {
+    clear();
+    session.save();
+    session.logout();
+    super.tearDown();
+  }
+  
+  public void clear () throws Exception {
+    Node rootNode = session.getRootNode();
+
+    Node documentNode = rootNode.getNode("Test.docx");
+    documentNode.remove();
+    session.save();
+  }
+
+}
