ee11d5f0946c61c7afc814922c4eb04e35598224
VladMartyshevskyi <vladmartishevskii@gmail.com>
2020-02-27 17:54:13 +0200

    Fixed path to .jsp for editor admin portlet

--- apps/portlet-editors/src/main/java/org/exoplatform/editors/portlet/EditorsAdminPortlet.java
+++ apps/portlet-editors/src/main/java/org/exoplatform/editors/portlet/EditorsAdminPortlet.java
@@ -12,7 +12,7 @@ public class EditorsAdminPortlet extends GenericPortlet {
 
   @Override
   public void doView(RenderRequest request, RenderResponse response) throws IOException, PortletException {
-    PortletRequestDispatcher prDispatcher = getPortletContext().getRequestDispatcher("/WEB-INF/pages/editorsadmin.jsp");
+    PortletRequestDispatcher prDispatcher = getPortletContext().getRequestDispatcher("/WEB-INF/editors-admin.jsp");
     prDispatcher.include(request, response);
   }
 }
