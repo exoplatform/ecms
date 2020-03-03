e414d4cd55104aabdcaca78ed4494749dfc7c861
Aleksandra Strelkova <aleksandrageor8@gmail.com>
2020-03-03 18:22:03 +0200

    fixed switchers, title, table styles, added translations

--- apps/portlet-editors/src/main/webapp/vue-apps/editorsAdmin/components/AdminApp.vue
+++ apps/portlet-editors/src/main/webapp/vue-apps/editorsAdmin/components/AdminApp.vue
@@ -1,64 +1,66 @@
 <template>
-  <v-app id="editors-admin" class="VuetifyApp" flat>
-    <v-container pa-0>
-      <v-layout row mx-0 class="white">
-        <v-flex d-flex xs12 px-3>
-          <v-layout mx-0 align-center>
-            <v-flex d-flex xs12>
-              <v-card flat color="transparent">
-                <a @click="navigateTo('tasks/my-task')">
-                  <v-card-text class="body-1 text-uppercase color-title px-0">
-                    {{ this.$t('editors.admin.title') }}
-                  </v-card-text>
-                </a>
-              </v-card>
-            </v-flex>
-          </v-layout>
-        </v-flex>
-      </v-layout>
-      <v-simple-table>
-        <template v-slot:default>
-          <thead>
-            <tr>
-              <th class="text-left">Provider</th>
-              <th class="text-left">Active</th>
-              <th class="text-left">Actions</th>
-            </tr>
-          </thead>
-          <tbody v-if="providers.length > 0">
-            <tr v-for="item in providers" :key="item.provider">
-              <td>{{ item.provider }}</td>
-              <td>
-                <v-switch
-                  :input-value="item.active"
-                  :label="`${item.active.toString()}`"
-                  @change="changeStatus(item)"
-                ></v-switch>
-              </td>
-              <td>
-                <v-btn text icon color="indigo" @click.stop="showDialog = true">
-                  <i class="material-icons">
-                    settings
-                  </i>
-                </v-btn>
-              </td>
-            </tr>
-          </tbody>
-        </template>
-      </v-simple-table>
-      <v-layout row justify-center>
-        <v-dialog v-model="showDialog" persistent max-width="600">
+  <v-app id="editors-admin" class="VuetifyApp">
+    <v-container style="width: 95%" class="v-application--is-ltr">
+      <v-row class="white">
+        <v-col xs12 px-3>
+          <h4 class="editorsTitle">
+            {{ $t('editors.admin.title') }}
+          </h4>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col xs12>
+          <v-simple-table>
+            <template v-slot:default>
+              <thead>
+                <tr>
+                  <th class="text-left">{{ $t('editors.admin.table.Provider') }}</th>
+                  <th class="text-left" style="width: 25%">{{ $t('editors.admin.table.Active') }}</th>
+                  <th class="text-left" style="width: 25%">{{ $t('editors.admin.table.Permissions') }}</th>
+                </tr>
+              </thead>
+              <tbody v-if="providers.length > 0">
+                <tr v-for="item in providers" :key="item.provider">
+                  <td>{{ item.provider }}</td>
+                  <td>
+                    <v-switch
+                      :input-value="item.active"
+                      class="v-input--switch--inset"
+                      @change="changeStatus(item)"
+                    ></v-switch>
+                  </td>
+                  <td>
+                    <v-btn text icon color="indigo" @click.stop="showDialog = true">
+                      <i class="material-icons">
+                        settings
+                      </i>
+                    </v-btn>
+                  </td>
+                </tr>
+              </tbody>
+            </template>
+          </v-simple-table>
+        </v-col>
+      </v-row>
+      <div class="text-center">
+        <v-dialog
+          v-model="showDialog"
+          width="600"
+        >
           <v-card>
-            <v-card-title class="headline grey lighten-2">Settings
+            <v-card-title
+              class="headline grey lighten-2"
+              primary-title
+            >
+              {{ this.$t('editors.admin.modal.title') }}
               <v-btn icon @click="showDialog = false">
                 <v-icon>close</v-icon>
               </v-btn>
             </v-card-title>
-            <v-card-text>
-            </v-card-text>
+            <v-card-text></v-card-text>
           </v-card>
         </v-dialog>
-      </v-layout>
+      </div>
     </v-container>
   </v-app>
 </template>
@@ -100,3 +102,21 @@ export default {
     }
 };
 </script>
+
+<style scoped>
+  .editorsTitle {
+    color: #4d5466;
+    font-size: 24px;
+    position: relative;
+    overflow: hidden;
+  }
+
+  .editorsTitle:after {
+    border-bottom: 1px solid #dadada;
+    height: 11px;
+    content: "";
+    position: absolute;
+    width: 100%;
+    margin-left: 10px;
+  }
+</style>
