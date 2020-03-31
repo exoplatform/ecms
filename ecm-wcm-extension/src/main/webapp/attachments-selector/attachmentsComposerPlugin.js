ba6c3bb20e8805026f4abfcd884960dbb13507f0
Thomas Delhoménie <tdelhomenie@exoplatform.com>
2020-03-31 15:48:09 +0200

    New composer - Update activity composer plugin (#560)
    * Update onExecute declaration
    * Define resource bundle

--- ecm-wcm-extension/src/main/webapp/attachments-selector/attachmentsComposerPlugin.js
+++ ecm-wcm-extension/src/main/webapp/attachments-selector/attachmentsComposerPlugin.js
@@ -1,12 +1,13 @@
 const ActivityComposerAttachmentsPlugin = {
   key: 'file',
   rank: 20,
+  resourceBundle: 'locale.attachmentsSelector.attachments',
   labelKey: 'attachments.composer.app.labelKey',
   description: 'attachments.composer.app.description',
   iconClass: 'addFileComposerIcon',
   appClass: 'attachmentsSelector',
   component: 'exo-attachments',
-  onExecute() {
+  onExecute: function() {
     document.getElementsByClassName('attachments drawer')[0].className += ' open';
   },
 };
