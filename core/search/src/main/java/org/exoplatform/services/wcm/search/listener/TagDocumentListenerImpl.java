439352642cce9d4fed6ed5fc7f563bcfb5520046
ahmed <achakroun@exoplatform.com>
2020-04-27 10:39:31 +0100

    Cleanup code

--- core/search/src/main/java/org/exoplatform/services/wcm/search/listener/TagDocumentListenerImpl.java
+++ core/search/src/main/java/org/exoplatform/services/wcm/search/listener/TagDocumentListenerImpl.java
@@ -1,6 +1,5 @@
 package org.exoplatform.services.wcm.search.listener;
 
-import org.exoplatform.commons.search.index.IndexingOperationProcessor;
 import org.exoplatform.commons.search.index.IndexingService;
 import org.exoplatform.services.listener.Event;
 import org.exoplatform.services.listener.Listener;
@@ -14,24 +13,17 @@ public class TagDocumentListenerImpl extends Listener<Node, String> {
 
   private static final Log LOG = ExoLogger.getExoLogger(TagDocumentListenerImpl.class);
 
-  private static final String TAG_ADDED_TO_DOCUMENT = "Document.event.TagAdded";
-
-  private static final String TAG_REMOVED_FROM_DOCUMENT = "Document.event.TagRemoved";
-
-  private IndexingOperationProcessor indexingOperationProcessor;
-
   private IndexingService indexingService;
 
-  public TagDocumentListenerImpl(IndexingOperationProcessor indexingOperationProcessor, IndexingService indexingService) {
-    this.indexingOperationProcessor = indexingOperationProcessor;
+  public TagDocumentListenerImpl(IndexingService indexingService) {
     this.indexingService = indexingService;
   }
 
   @Override
   public void onEvent(Event<Node, String> event) throws Exception {
-
     Node node = event.getSource();
-    if (TAG_ADDED_TO_DOCUMENT.equals(event.getEventName()) || TAG_REMOVED_FROM_DOCUMENT.equals(event.getEventName())) {
+    LOG.debug("Notifying reindexing service for tag document node={}", node.getUUID());
+    if (node!= null) {
       indexingService.reindex(FileindexingConnector.TYPE, node.getUUID());
     }
   }
