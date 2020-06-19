30fe97acece3d9470959826bca965af5ca045af9
Romain Dénarié <romain.denarie@exoplatform.com>
2020-06-19 11:32:13 +0200

    Update content viewer rest url

--- ecm-wcm-extension/src/main/webapp/javascript/eXo/ecm/document-preview.js
+++ ecm-wcm-extension/src/main/webapp/javascript/eXo/ecm/document-preview.js
@@ -1072,7 +1072,7 @@
         this.show();
       }
 
-      docContentContainer.load('portal/rest/contentviewer/' + this.settings.doc.repository + '/' + this.settings.doc
+      docContentContainer.load('/portal/rest/contentviewer/' + this.settings.doc.repository + '/' + this.settings.doc
       .workspace + '/' + this.settings.doc.id, function() {
         $('.loading', docContentContainer).hide();
         resizeEventHandler();
