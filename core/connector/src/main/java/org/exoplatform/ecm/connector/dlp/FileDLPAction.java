88233d17050961381aeb05b3040f49d2e22ab7b6
Ayoub Zayati <ayoub.ezzayati@exoplatform.com>
2020-12-14 11:41:28 +0100

    Task-37622: Implement Dlp feature V0

---
+++ core/connector/src/main/java/org/exoplatform/ecm/connector/dlp/FileDLPAction.java
@@ -0,0 +1,80 @@
+package org.exoplatform.ecm.connector.dlp;
+
+import javax.jcr.observation.Event;
+
+import org.apache.commons.chain.Context;
+
+import org.exoplatform.commons.api.settings.ExoFeatureService;
+import org.exoplatform.commons.dlp.queue.QueueDlpService;
+import org.exoplatform.commons.utils.CommonsUtils;
+import org.exoplatform.services.cms.documents.TrashService;
+import org.exoplatform.services.ext.action.InvocationContext;
+import org.exoplatform.services.jcr.impl.core.NodeImpl;
+import org.exoplatform.services.jcr.impl.core.PropertyImpl;
+import org.exoplatform.services.jcr.impl.ext.action.AdvancedAction;
+import org.exoplatform.services.jcr.impl.ext.action.AdvancedActionException;
+import org.exoplatform.services.log.ExoLogger;
+import org.exoplatform.services.log.Log;
+import org.exoplatform.services.wcm.core.NodetypeConstant;
+
+/**
+ * JCR action which listens on all nodes events to validate them
+ */
+public class FileDLPAction implements AdvancedAction {
+  private static final Log LOGGER = ExoLogger.getExoLogger(FileDLPAction.class);
+  
+  private static final String DLP_FEATURE = "dlp";
+
+  private TrashService     trashService;
+
+  private QueueDlpService queueDlpService;
+  
+  private ExoFeatureService featureService;
+
+  public FileDLPAction() {
+    this.trashService = CommonsUtils.getService(TrashService.class);
+    this.queueDlpService = CommonsUtils.getService(QueueDlpService.class);
+    this.featureService = CommonsUtils.getService(ExoFeatureService.class);
+  }
+
+  @Override
+  public boolean execute(Context context) throws Exception {
+    if (!featureService.isActiveFeature(DLP_FEATURE)) {
+      return false;
+    }
+    int eventType = (Integer) context.get(InvocationContext.EVENT);
+    NodeImpl node;
+    switch(eventType) {
+      case Event.NODE_ADDED:
+        node = (NodeImpl) context.get(InvocationContext.CURRENT_ITEM);
+        if(node != null && !trashService.isInTrash(node)) {
+          String entityId = node.getInternalIdentifier();
+          queueDlpService.addToQueue(FileDlpConnector.TYPE, entityId);
+        }
+        break;
+      case Event.PROPERTY_ADDED:
+      case Event.PROPERTY_CHANGED:
+      case Event.PROPERTY_REMOVED:
+        PropertyImpl property = (PropertyImpl) context.get(InvocationContext.CURRENT_ITEM);
+        if(property != null) {
+          node = property.getParent();
+          if (node != null && !trashService.isInTrash(node)) {
+            if (node.isNodeType(NodetypeConstant.NT_RESOURCE)) {
+              node = node.getParent();
+            }
+            if (node.isNodeType(NodetypeConstant.NT_FILE)) {
+              String entityId = node.getInternalIdentifier();
+              queueDlpService.addToQueue(FileDlpConnector.TYPE, entityId);
+            }
+          }
+        }
+        break;
+    }
+    return true;
+  }
+  
+  @Override
+  public void onError(Exception e, Context context) throws AdvancedActionException {
+    LOGGER.error("Error while validating file", e);
+  }
+}
