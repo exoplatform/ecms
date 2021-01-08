7acaafb49156aa35cd9d3fba4e97be485be65e2b
mzorai <mzorai@exoplatform.com>
2021-01-08 09:50:22 +0100

    fix/TASK-38700: Fix document long title truncation (#1066) (#1069)
    (cherry picked from commit d3200ec6309f553a520ce120a36fbd2df5ebd106)
    
    Co-authored-by: aelbenney <42932906+aelbenney@users.noreply.github.com>
    (cherry picked from commit d9a65e406db97619e4383b1352806d5434eb2253)

--- apps/portlet-documents/src/main/webapp/vue-app/common/components/ExoDocuments.vue
+++ apps/portlet-documents/src/main/webapp/vue-app/common/components/ExoDocuments.vue
@@ -1,26 +1,14 @@
 <template>
-  <v-container pa-0>
-    <v-layout
-      row
-      mx-0
-      class="white">
-      <v-flex
-        xs12>
-        <v-layout
-          row
-          mx-0>
-          <v-list>
-            <v-list-item-group color="primary">
-              <exo-document
-                v-for="(document,i) in documents"
-                :key="i"
-                :document="document" />
-            </v-list-item-group>
-          </v-list>
-        </v-layout>
-      </v-flex>
-    </v-layout>
-  </v-container>
+  <v-flex>
+    <v-list class="documents-timeline-list">
+      <v-list-item-group color="primary">
+        <exo-document
+          v-for="(document,i) in documents"
+          :key="i"
+          :document="document"/>
+      </v-list-item-group>
+    </v-list>
+  </v-flex>
 </template>
 <script>
   export default {
