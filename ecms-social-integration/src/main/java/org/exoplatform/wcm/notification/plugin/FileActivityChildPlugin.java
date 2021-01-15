d4c5258de76e596a11fb439d61354ef48509ee40
hakermi <70575401+hakermi@users.noreply.github.com>
2021-01-15 13:14:15 +0100

    MAINT-36333 : File activity child plugin Refactor (#1075)
    * FileActivityChild Refactor
    
    * correct activity link
    
    * update plugin
    
    * update plugin 2

--- ecms-social-integration/src/main/java/org/exoplatform/wcm/notification/plugin/FileActivityChildPlugin.java
+++ ecms-social-integration/src/main/java/org/exoplatform/wcm/notification/plugin/FileActivityChildPlugin.java
@@ -48,7 +48,6 @@ import org.exoplatform.social.core.identity.provider.SpaceIdentityProvider;
 import org.exoplatform.social.core.manager.ActivityManager;
 import org.exoplatform.social.core.manager.IdentityManager;
 import org.exoplatform.social.core.space.spi.SpaceService;
-import org.exoplatform.social.notification.LinkProviderUtils;
 import org.exoplatform.wcm.ext.component.activity.FileUIActivity;
 import org.exoplatform.wcm.ext.component.activity.listener.Utils;
 import org.exoplatform.webui.cssfile.*;
@@ -72,7 +71,6 @@ public class FileActivityChildPlugin extends AbstractNotificationChildPlugin {
   public static final String EXO_RESOURCES_URI            = "/eXoSkin/skin/images/themes/default/Icons/TypeIcons/EmailNotificationIcons/";
   public static final String DOCNAME                      = "DOCNAME";
   public static final String ICON_FILE_EXTENSION          = ".png";
-  public static final String CONTENT_LINK                 = "contentLink";
 
   private String[]             mimeType;
   private String[]             nodeUUID;
