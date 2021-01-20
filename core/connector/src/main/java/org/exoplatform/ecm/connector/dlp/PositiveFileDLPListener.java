e8d78c8bce80684a6ec6c1d31e921c7cb9e37c18
ahmed <achakroun@exoplatform.com>
2021-01-20 16:54:17 +0100

    Clean code

--- core/connector/src/main/java/org/exoplatform/ecm/connector/dlp/PositiveFileDLPListener.java
+++ core/connector/src/main/java/org/exoplatform/ecm/connector/dlp/PositiveFileDLPListener.java
@@ -1,49 +1,23 @@
 package org.exoplatform.ecm.connector.dlp;
 
+import org.exoplatform.commons.dlp.connector.DlpServiceConnector;
+import org.exoplatform.commons.dlp.processor.DlpOperationProcessor;
 import org.exoplatform.commons.dlp.service.DlpPositiveItemService;
 import org.exoplatform.commons.utils.CommonsUtils;
-import org.exoplatform.services.jcr.RepositoryService;
-import org.exoplatform.services.jcr.core.ExtendedSession;
 import org.exoplatform.services.listener.Event;
 import org.exoplatform.services.listener.Listener;
-import org.exoplatform.services.log.ExoLogger;
-import org.exoplatform.services.log.Log;
-import org.exoplatform.services.wcm.utils.WCMCoreUtils;
-
-import javax.jcr.Node;
 
 public class PositiveFileDLPListener extends Listener<DlpPositiveItemService, String> {
-  public static final String DLP_SECURITY_FOLDER = "Security";
 
-  private static final String COLLABORATION_WS = "collaboration";
-
-  private static final Log LOGGER = ExoLogger.getExoLogger(PositiveFileDLPListener.class);
-
-  private RepositoryService repositoryService;
+  private DlpOperationProcessor dlpOperationProcessor;
 
   public PositiveFileDLPListener() {
-    this.repositoryService = CommonsUtils.getService(RepositoryService.class);
+    dlpOperationProcessor = CommonsUtils.getService(DlpOperationProcessor.class);
   }
 
   public void onEvent(Event<DlpPositiveItemService, String> event) throws Exception {
-
-    if ("dlp.listener.event.delete.document".equals(event.getEventName())) {
-      ExtendedSession session = null;
-      try {
-        String entityId = event.getData();
-        session =
-            (ExtendedSession) WCMCoreUtils.getSystemSessionProvider()
-                                          .getSession(COLLABORATION_WS, repositoryService.getCurrentRepository());
-        Node dlpSecurityNode = (Node) session.getItem("/" + DLP_SECURITY_FOLDER);
-        Node node = session.getNodeByIdentifier(entityId);
-
-        if (node != null && dlpSecurityNode != null) {
-          node.remove();
-          dlpSecurityNode.save();
-        }
-      } catch (Exception e) {
-        LOGGER.error("Error while deleting dlp file item", e);
-      }
-    }
+    DlpServiceConnector fileConnector = (DlpServiceConnector) dlpOperationProcessor.getConnectors().get(FileDlpConnector.TYPE);
+    fileConnector.removePositiveItemNode(event.getData());
   }
+
 }
