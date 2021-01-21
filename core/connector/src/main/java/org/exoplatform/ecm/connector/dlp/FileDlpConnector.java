0c1e2bebb2fb9be385c136b468ad18ba50e82ad0
Ahmed <achakroun@exoplatform.com>
2021-01-21 12:41:08 +0100

    Task-37847 : Active Dlp delete action (#1081)

--- core/connector/src/main/java/org/exoplatform/ecm/connector/dlp/FileDlpConnector.java
+++ core/connector/src/main/java/org/exoplatform/ecm/connector/dlp/FileDlpConnector.java
@@ -152,4 +152,23 @@ public class FileDlpConnector extends DlpServiceConnector {
     dlpPositiveItemEntity.setKeywords(dlpKeywords);
     dlpPositiveItemService.addDlpPositiveItem(dlpPositiveItemEntity);
   }
+
+  @Override
+  public void removePositiveItem(String itemReference) {
+    ExtendedSession session = null;
+    try {
+      session =
+          (ExtendedSession) WCMCoreUtils.getSystemSessionProvider()
+                                        .getSession(COLLABORATION_WS, repositoryService.getCurrentRepository());
+      Node dlpSecurityNode = (Node) session.getItem("/" + DLP_SECURITY_FOLDER);
+      Node node = session.getNodeByIdentifier(itemReference);
+
+      if (node != null && dlpSecurityNode != null) {
+        node.remove();
+        dlpSecurityNode.save();
+      }
+    } catch (Exception e) {
+      LOGGER.error("Error while deleting dlp file item", e);
+    }
+  }
 }
