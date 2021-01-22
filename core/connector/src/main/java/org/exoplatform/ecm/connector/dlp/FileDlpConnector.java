8cd3fb631d774e92c26ad65c22068b506f482141
ahmed <achakroun@exoplatform.com>
2021-01-22 14:40:00 +0100

    Add getUrlItem method

--- core/connector/src/main/java/org/exoplatform/ecm/connector/dlp/FileDlpConnector.java
+++ core/connector/src/main/java/org/exoplatform/ecm/connector/dlp/FileDlpConnector.java
@@ -14,11 +14,7 @@ import org.exoplatform.commons.search.index.IndexingService;
 import org.exoplatform.commons.utils.CommonsUtils;
 import org.exoplatform.commons.dlp.queue.QueueDlpService;
 import org.exoplatform.commons.search.index.IndexingService;
-import org.exoplatform.container.ExoContainer;
-import org.exoplatform.container.ExoContainerContext;
-import org.exoplatform.container.definition.PortalContainerConfig;
 import org.exoplatform.container.xml.InitParams;
-import org.exoplatform.container.xml.PortalContainerInfo;
 import org.exoplatform.container.xml.ValueParam;
 import org.exoplatform.services.jcr.RepositoryService;
 import org.exoplatform.services.jcr.core.ExtendedSession;
@@ -30,8 +26,6 @@ import org.exoplatform.web.controller.metadata.ControllerDescriptor;
 import org.exoplatform.web.controller.router.Router;
 import org.exoplatform.web.controller.router.RouterConfigException;
 
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
 import java.util.Collections;
 
 import java.util.Calendar;
@@ -40,29 +34,29 @@ import java.util.Calendar;
  * Dlp Connector for Files
  */
 public class FileDlpConnector extends DlpServiceConnector {
-  
-  public static final String  TYPE                 = "file";
-  
-  public static final String DLP_SECURITY_FOLDER   = "Security";
 
-  private static final Log    LOGGER               = ExoLogger.getExoLogger(FileDlpConnector.class);
+  public static final String TYPE = "file";
 
-  private static final String COLLABORATION_WS     = "collaboration";
+  public static final String DLP_SECURITY_FOLDER = "Security";
 
-  private static final String DLP_KEYWORDS_PARAM   = "dlp.keywords";
+  private static final Log LOGGER = ExoLogger.getExoLogger(FileDlpConnector.class);
 
-  private static final String TITLE                = "exo:title";
+  private static final String COLLABORATION_WS = "collaboration";
 
-  private static final String OWNER                = "exo:owner";
+  private static final String DLP_KEYWORDS_PARAM = "dlp.keywords";
 
-  private RepositoryService   repositoryService;
-  
+  private static final String TITLE = "exo:title";
+
+  private static final String OWNER = "exo:owner";
+
+  private RepositoryService repositoryService;
+
   private IndexingService indexingService;
 
-  private String              dlpKeywords;
-  
+  private String dlpKeywords;
+
   private FileSearchServiceConnector fileSearchServiceConnector;
-  
+
   private QueueDlpService queueDlpService;
 
   public FileDlpConnector(InitParams initParams, FileSearchServiceConnector fileSearchServiceConnector,
@@ -71,12 +65,12 @@ public class FileDlpConnector extends DlpServiceConnector {
     ValueParam dlpKeywordsParam = initParams.getValueParam(DLP_KEYWORDS_PARAM);
     this.dlpKeywords = dlpKeywordsParam.getValue();
     if (dlpKeywords != null) {
-      dlpKeywords=dlpKeywords.replace(","," ");
+      dlpKeywords = dlpKeywords.replace(",", " ");
     }
     this.repositoryService = repositoryService;
     this.indexingService = indexingService;
-    this.fileSearchServiceConnector=fileSearchServiceConnector;
-    this.queueDlpService=queueDlpService;
+    this.fileSearchServiceConnector = fileSearchServiceConnector;
+    this.queueDlpService = queueDlpService;
   }
 
   @Override
@@ -106,13 +100,13 @@ public class FileDlpConnector extends DlpServiceConnector {
       searchContext = new SearchContext(new Router(new ControllerDescriptor()), "");
       return dlpKeywords != null
           && !dlpKeywords.isEmpty()
-          && fileSearchServiceConnector.dlpSearch(searchContext,dlpKeywords,entityId).size()>0;
+          && fileSearchServiceConnector.dlpSearch(searchContext, dlpKeywords, entityId).size() > 0;
     } catch (Exception ex) {
       LOGGER.error("Can not create SearchContext", ex);
     }
     return false;
   }
-  
+
   @VisibleForTesting
   protected void treatItem(String entityId) {
     ExtendedSession session = null;
@@ -123,16 +117,16 @@ public class FileDlpConnector extends DlpServiceConnector {
       Node node = session.getNodeByIdentifier(entityId);
       Node dlpSecurityNode = (Node) session.getItem("/" + DLP_SECURITY_FOLDER);
       String fileName = node.getName();
-      if (!node.getPath().startsWith("/"+DLP_SECURITY_FOLDER+"/")) {
+      if (!node.getPath().startsWith("/" + DLP_SECURITY_FOLDER + "/")) {
         workspace.move(node.getPath(), "/" + DLP_SECURITY_FOLDER + "/" + fileName);
         indexingService.unindex(TYPE, entityId);
         long endTime = System.currentTimeMillis();
         long totalTime = endTime - startTime;
         LOGGER.info("service={} operation={} parameters=\"fileName:{}\" status=ok " + "duration_ms={}",
-                 DlpOperationProcessor.DLP_FEATURE,
-                 DLP_POSITIVE_DETECTION,
-                 fileName,
-                 totalTime);
+                    DlpOperationProcessor.DLP_FEATURE,
+                    DLP_POSITIVE_DETECTION,
+                    fileName,
+                    totalTime);
         saveDlpPositiveItem(node);
       }
     } catch (Exception e) {
@@ -140,7 +134,7 @@ public class FileDlpConnector extends DlpServiceConnector {
     }
   }
 
-  private void saveDlpPositiveItem(Node node) throws Exception {
+  private void saveDlpPositiveItem(Node node) throws RepositoryException {
     DlpPositiveItemService dlpPositiveItemService = CommonsUtils.getService(DlpPositiveItemService.class);
     DlpPositiveItemEntity dlpPositiveItemEntity = new DlpPositiveItemEntity();
     dlpPositiveItemEntity.setReference(node.getUUID());
@@ -156,60 +150,8 @@ public class FileDlpConnector extends DlpServiceConnector {
     dlpPositiveItemEntity.setDetectionDate(Calendar.getInstance());
     // to be updated with detected keyword
     dlpPositiveItemEntity.setKeywords(dlpKeywords);
-    dlpPositiveItemEntity.setDownloadUrl(getDownloadRestServiceLink(node));
-
     dlpPositiveItemService.addDlpPositiveItem(dlpPositiveItemEntity);
   }
-
-  /**
-   * @param node nt:file node with have the data stream
-   * @return Link to download the jcr:data of the given node
-   * @throws Exception
-   */
-  private static String getDownloadRestServiceLink(Node node) throws Exception {
-    ExoContainer container = ExoContainerContext.getCurrentContainer();
-    PortalContainerInfo containerInfo = (PortalContainerInfo) container.getComponentInstanceOfType(PortalContainerInfo.class);
-    String portalName = containerInfo.getContainerName();
-    PortalContainerConfig portalContainerConfig = (PortalContainerConfig) container.getComponentInstance(PortalContainerConfig.class);
-    String restContextName = portalContainerConfig.getRestContextName(portalName);
-    StringBuilder sb = new StringBuilder();
-    Node currentNode = getRealNode(node);
-    String ndPath = currentNode.getPath();
-    if (ndPath.startsWith("/")) {
-      ndPath = ndPath.substring(1);
-    }
-    String encodedPath = encodePath(ndPath,"UTF-8");
-    sb.append("/").append(restContextName).append("/contents/download/");
-    sb.append(currentNode.getSession().getWorkspace().getName()).append("/").append(encodedPath);
-    if (node.isNodeType("nt:frozenNode")) {
-      sb.append("?version=" + node.getParent().getName());
-    }
-    return sb.toString();
-  }
-
-  /**
-   * Get the real node from frozen node, symlink node return True if the node is
-   * viewable, otherwise will return False
-   *
-   * @param node: The node to check
-   */
-  private static Node getRealNode(Node node) throws Exception {
-    // TODO: Need to add to check symlink node
-    if (node.isNodeType("nt:frozenNode")) {
-      String uuid = node.getProperty("jcr:frozenUuid").getString();
-      return node.getSession().getNodeByUUID(uuid);
-    }
-    return node;
-  }
-
-  private static String encodePath(String path, String encoding) {
-    try {
-      String encodedPath = URLEncoder.encode(path, encoding);
-      encodedPath = encodedPath.replaceAll("%2F","/");
-      return encodedPath;
-    } catch (UnsupportedEncodingException e){
-      LOGGER.error("Failed to encode path '" + path + "' with encoding '" + encoding + "'",e);
-    }
 
   @Override
   public void removePositiveItem(String itemReference) {
@@ -227,5 +169,21 @@ public class FileDlpConnector extends DlpServiceConnector {
       }
     } catch (Exception e) {
       LOGGER.error("Error while deleting dlp file item", e);
+    }
+  }
+
+  @Override
+  public String getItemUrl(String itemReference) {
+    ExtendedSession session = null;
+    try {
+      session =
+          (ExtendedSession) WCMCoreUtils.getSystemSessionProvider()
+                                        .getSession(COLLABORATION_WS, repositoryService.getCurrentRepository());
+      Node node = session.getNodeByIdentifier(itemReference);
+      return WCMCoreUtils.getLinkInDocumentsApplication(node.getPath());
+    } catch (Exception e) {
+      LOGGER.error("Error while deleting dlp file item", e);
+    }
+    return null;
   }
 }
