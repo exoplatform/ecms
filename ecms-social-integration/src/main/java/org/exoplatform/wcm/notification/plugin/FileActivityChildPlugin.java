aaf87cc7968bce84d7eb7321b11ba52ad78a106d
Ali HAMDI <ahamdi@exoplatform.com>
2021-01-26 10:51:49 +0100

    Update ecms-social-integration/src/main/java/org/exoplatform/wcm/notification/plugin/FileActivityChildPlugin.java
    Co-authored-by: Houssem B. Ali <hbenali@exoplatform.com>

--- ecms-social-integration/src/main/java/org/exoplatform/wcm/notification/plugin/FileActivityChildPlugin.java
+++ ecms-social-integration/src/main/java/org/exoplatform/wcm/notification/plugin/FileActivityChildPlugin.java
@@ -46,12 +46,9 @@ import org.exoplatform.social.core.activity.model.ExoSocialActivity;
 import org.exoplatform.social.core.identity.model.Identity;
 import org.exoplatform.social.core.identity.provider.SpaceIdentityProvider;
 import org.exoplatform.social.core.manager.ActivityManager;
-<<<<<<< HEAD
-=======
 import org.exoplatform.social.core.manager.IdentityManager;
 import org.exoplatform.social.core.space.spi.SpaceService;
 import org.exoplatform.social.notification.LinkProviderUtils;
->>>>>>> 9f5c8c9829... MAINT-36333 : File activity child plugin Refactor (#1075) (#1088)
 import org.exoplatform.wcm.ext.component.activity.FileUIActivity;
 import org.exoplatform.wcm.ext.component.activity.listener.Utils;
 import org.exoplatform.webui.cssfile.*;
