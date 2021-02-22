0cd481ad83622d0187df11f4f1554151c64bd613
mzorai <mzorai@exoplatform.com>
2021-02-22 17:07:01 +0100

    MAINT-39705 : Upload new version  is not working when the file is renamed with with a space in the name (#1150)
    * MAINT-39705 : Upload new version  is not working when the file is renamed with with a space in the name
    
    **The problem** when uploading a new version does not work with a filename that contains space so **the solution** is to decode the file name.
    
    * Upload new version is not working when the file contains a space in its name (#1144)
    
    Co-authored-by: Rdidmohamedelhadi <54801286+Rdidmohamedelhadi@users.noreply.github.com>

--- core/connector/src/main/java/org/exoplatform/wcm/connector/FileUploadHandler.java
+++ core/connector/src/main/java/org/exoplatform/wcm/connector/FileUploadHandler.java
@@ -277,7 +277,7 @@ public class FileUploadHandler {
     DocumentBuilder builder = factory.newDocumentBuilder();
     Document fileExistence = builder.newDocument();
     fileName = Text.escapeIllegalJcrChars(fileName);
-    fileName = Utils.cleanNameWithAccents(fileName);
+    fileName = URLDecoder.decode(URLDecoder.decode(fileName,"UTF-8"),"UTF-8");
     Element rootElement = fileExistence.createElement(
                               parent.hasNode(fileName) ? "Existed" : "NotExisted");
     if(parent.hasNode(fileName)){
@@ -452,8 +452,6 @@ public class FileUploadHandler {
       Node jcrContent=null;
       boolean fileCreated = false;
       String exoTitle = URLDecoder.decode(fileName, "UTF-8");
-      
-      fileName = Utils.cleanNameWithAccents(fileName);
       DMSMimeTypeResolver mimeTypeResolver = DMSMimeTypeResolver.getInstance();
       String mimetype = mimeTypeResolver.getMimeType(resource.getFileName());
       String nodeName = fileName;
