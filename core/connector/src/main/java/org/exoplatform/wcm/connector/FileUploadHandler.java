fb59c32b3d6477d110c1c84b9297af18bd0aa6ae
Moez Diha <38471028+mdiha@users.noreply.github.com>
2021-03-09 09:16:27 +0100

    MAINT-41089: Make sure that the title of uploaded file with special characters is not encoded. (#1175)
    When uploading a file that it's title contains special characters it is changed by replacing the special characters by the character (-) due to JCR limitation, these changes are required for the created node name property but it is not required for the title so i made sure to apply these changes only for the file's node name by keeping its title as the original value.

--- core/connector/src/main/java/org/exoplatform/wcm/connector/FileUploadHandler.java
+++ core/connector/src/main/java/org/exoplatform/wcm/connector/FileUploadHandler.java
@@ -394,6 +394,8 @@ public class FileUploadHandler {
                                String userId,
                                String existenceAction,
                                boolean isNewVersion) throws Exception {
+    String exoTitle = fileName;
+    fileName = Utils.cleanName(fileName);
     try {
       CacheControl cacheControl = new CacheControl();
       cacheControl.setNoCache(true);
@@ -451,7 +453,6 @@ public class FileUploadHandler {
       Node file = null;
       Node jcrContent=null;
       boolean fileCreated = false;
-      String exoTitle = URLDecoder.decode(fileName, "UTF-8");
       DMSMimeTypeResolver mimeTypeResolver = DMSMimeTypeResolver.getInstance();
       String mimetype = mimeTypeResolver.getMimeType(resource.getFileName());
       String nodeName = fileName;
