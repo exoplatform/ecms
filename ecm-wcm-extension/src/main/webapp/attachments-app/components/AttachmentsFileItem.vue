03ac1968f70ed2aee72457ae625d562079bbdc2d
aelbenney <42932906+aelbenney@users.noreply.github.com>
2021-04-05 14:56:31 +0100

    TASK-41656: add new Attachments App in snapshot page

---
+++ ecm-wcm-extension/src/main/webapp/attachments-app/components/AttachmentsFileItem.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="attachment">
+    <v-list-item-avatar class="rounded-lg">
+      <div class="fileType">
+        <i :class="getIconClassFromFileMimeType(file.mimetype)"></i>
+      </div>
+    </v-list-item-avatar>
+
+    <v-list-item-content class="serverFileContent align-center">
+      <v-list-item-title class="serverFileTitle'">{{ file.name }}</v-list-item-title>
+    </v-list-item-content>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    file: {
+      type: Object,
+      default: () => null,
+    }
+  },
+  methods: {
+    getIconClassFromFileMimeType: function (fileMimeType) {
+      if (fileMimeType) {
+        const fileMimeTypeClass = fileMimeType.replace(/\./g, '').replace('/', '').replace('\\', '');
+        return this.file.isCloudFile
+          ? `uiIcon32x32${fileMimeType.replace(/[/.]/g, '')}`
+          : `uiIconFileType${fileMimeTypeClass} uiIconFileTypeDefault`;
+      } else {
+        return 'uiIconFileTypeDefault';
+      }
+    }
+  }
+};
+</script>
