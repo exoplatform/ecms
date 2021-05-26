3a443c3203338289c1005b58de4e87da6992b3d5
El kamel Hassen <helkamel@exoplatform.com>
2021-05-26 17:27:34 +0100

    Task-43709: implement share document drawer

--- ecm-wcm-extension/webpack.common.js
+++ ecm-wcm-extension/webpack.common.js
@@ -5,7 +5,8 @@ let config = {
   // set the entry point of the application
   // can use multiple entry
   entry: {
-    attachmentsSelector: './src/main/webapp/attachments-selector/main.js'
+    shareDocumentActivity: './src/main/webapp/share-activity-document/main.js',
+    attachmentsSelector: './src/main/webapp/attachments-selector/main.js',
   },
   module: {
     rules: [
