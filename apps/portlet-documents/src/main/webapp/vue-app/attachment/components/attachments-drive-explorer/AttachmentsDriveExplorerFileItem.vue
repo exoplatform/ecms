7e806b845d7ca2ddd024e64ad1fff98d4c49e962
aelbenney <42932906+aelbenney@users.noreply.github.com>
2021-06-09 12:14:54 +0100

    TASK-44885: Add check icon for selected files in drive explorer (#1299)

--- apps/portlet-documents/src/main/webapp/vue-app/attachment/components/attachments-drive-explorer/AttachmentsDriveExplorerFileItem.vue
+++ apps/portlet-documents/src/main/webapp/vue-app/attachment/components/attachments-drive-explorer/AttachmentsDriveExplorerFileItem.vue
@@ -5,8 +5,16 @@
       rel="tooltip"
       class="fileTitle d-flex flex-column v-messages"
       data-placement="bottom">
-      <div class="fileType center">
-        <i :class="getIconClassFromFileMimeType(file.mimetype)"></i>
+      <div :class="file.isSelected && 'selected'" class="fileType d-flex">
+        <div class="file-type-icon pa-0 col-9 text-right pt-2">
+          <i :class="getIconClassFromFileMimeType(file.mimetype)"></i>
+        </div>
+        <v-icon
+          v-show="file.isSelected"
+          color="primary"
+          class="align-self-start pe-1"
+          size="18">fas fa-check-square
+        </v-icon>
       </div>
       <div class="selectionLabel text-truncate text-color center">{{ file.name }}</div>
     </a>
