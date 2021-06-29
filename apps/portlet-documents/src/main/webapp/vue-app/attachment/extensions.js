8860228478fac97a3f8d141db5d88be0db4cc836
Boubaker Khanfir <bkhanfir@exoplatform.com>
2021-06-29 13:29:29 +0100

    TASK-46356 Avoid displaying File attachment in OO version comment (#1333)

--- apps/portlet-documents/src/main/webapp/vue-app/attachment/extensions.js
+++ apps/portlet-documents/src/main/webapp/vue-app/attachment/extensions.js
@@ -29,8 +29,8 @@ export function installExtensions() {
   extensionRegistry.registerComponent('ActivityContent', 'activity-content-extensions', {
     id: 'attachments',
     isEnabled: (params) => {
-      const activity = params && params.activity;
-      return activity && activity.templateParams && (activity.templateParams.DOCPATH || activity.templateParams.nodePath);
+      const activityOrComment = params && params.activity;
+      return activityOrComment && !activityOrComment.activityId && activityOrComment.templateParams && (activityOrComment.templateParams.DOCPATH || activityOrComment.templateParams.nodePath);
     },
     vueComponent: Vue.options.components['activity-attachments'],
     rank: 10,
