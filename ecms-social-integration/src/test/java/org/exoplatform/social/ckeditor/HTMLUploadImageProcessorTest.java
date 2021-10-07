0af344d00318a7d877437e26478b45daf284170e
Mohamed Amine Krout <mkrout@exoplatform.com>
2021-10-07 12:52:54 +0100

    Task-49179 : Import Notes

--- ecms-social-integration/src/test/java/org/exoplatform/social/ckeditor/HTMLUploadImageProcessorTest.java
+++ ecms-social-integration/src/test/java/org/exoplatform/social/ckeditor/HTMLUploadImageProcessorTest.java
@@ -3,12 +3,14 @@ package org.exoplatform.social.ckeditor;
 import static org.junit.Assert.*;
 import static org.mockito.Matchers.*;
 import static org.mockito.Mockito.*;
+import static org.powermock.api.mockito.PowerMockito.whenNew;
 
 import javax.jcr.Node;
 import javax.jcr.Property;
 import javax.jcr.Session;
 import javax.jcr.Workspace;
 
+import org.apache.commons.io.IOUtils;
 import org.exoplatform.ecms.uploads.HTMLUploadImageProcessorImpl;
 import org.exoplatform.services.jcr.config.RepositoryEntry;
 import org.exoplatform.services.jcr.core.ManageableRepository;
@@ -30,6 +32,7 @@ import org.exoplatform.upload.UploadResource;
 import org.exoplatform.upload.UploadService;
 
 import java.io.*;
+import java.nio.file.Files;
 
 @RunWith(MockitoJUnitRunner.class)
 public class HTMLUploadImageProcessorTest {
@@ -155,6 +158,45 @@ public class HTMLUploadImageProcessorTest {
   }
 
   @Test
+  public void shouldReturnUpdatedContentWhenEmbeddedImportedImageForSpace() throws Exception {
+    // Given
+    HTMLUploadImageProcessorImpl imageProcessor = new HTMLUploadImageProcessorImpl(portalContainer, uploadService, repositoryService, linkManager, sessionProviderService,nodeHierarchyCreator,wcmService);
+    String content = "<p>content with image: <img src=\"//-image.png-//\" /></p>";
+    Node node = mock(Node.class);
+    when(node.hasNode(eq("image.png"))).thenReturn(false);
+    when(node.addNode(anyString(), anyString())).thenReturn(node);
+    when(portalContainer.getName()).thenReturn("portal");
+    when(portalContainer.getRestContextName()).thenReturn("rest");
+    when(repositoryService.getCurrentRepository()).thenReturn(repository);
+    when(repository.getConfiguration()).thenReturn(repositoryEntry);
+    when(sessionProviderService.getSystemSessionProvider(null)).thenReturn(sessionProvider);
+    when(repositoryEntry.getName()).thenReturn("repository");
+    when(sessionProvider.getSession("collaboration",repository)).thenReturn(session);
+    when(node.getSession()).thenReturn(session);
+    when(node.getUUID()).thenReturn("123524");
+    Workspace workspace = mock(Workspace.class);
+    when(session.getWorkspace()).thenReturn(workspace);
+    when(session.getRootNode()).thenReturn(node);
+    when(node.getNode(anyString())).thenReturn(node);
+    when(workspace.getName()).thenReturn("collaboration");
+
+
+    lenient().when(node.getPath()).thenReturn("/path/to/image.png");
+    File file = new File(System.getProperty("java.io.tmpdir") + File.separator +"image.png");
+    try (OutputStream outputStream = new FileOutputStream(file)) {
+      IOUtils.copy(new ByteArrayInputStream("test data".getBytes()), outputStream);
+    } catch (Exception e) {
+      throw new IllegalArgumentException("Cannot create the file", e);
+    }
+
+    // When
+    String processedContent = imageProcessor.processSpaceImages(content, "nodeParent", null);
+
+    // Then
+    assertTrue(processedContent.matches("<p>content with image: <img src=\"/portal/rest/images/repository/collaboration/[a-z0-9]+\" /></p>"));
+  }
+
+  @Test
   public void shouldReturnUpdatedContentWhenEmbeddedImageForUser() throws Exception {
     // Given
     HTMLUploadImageProcessorImpl imageProcessor = new HTMLUploadImageProcessorImpl(portalContainer, uploadService, repositoryService, linkManager, sessionProviderService,nodeHierarchyCreator,wcmService);
@@ -194,23 +236,6 @@ public class HTMLUploadImageProcessorTest {
     String content = "<p>content with image: <img src=\"/portal/rest/images/repository/collaboration/123456\" /></p>";
     Node node = mock(Node.class);
     Property property = mock(Property.class);
-    InputStream inputStream = mock(InputStream.class);
-  /*  File imageFile = uploadFolder.newFile("image.png");
-    UploadResource uploadImage = new UploadResource("123456", "image.png");
-    uploadImage.setStoreLocation(imageFile.getPath());
-    when(uploadService.getUploadResource(eq("123456"))).thenReturn(uploadImage);
-    when(node.hasNode(eq("image.png"))).thenReturn(false);
-    when(node.addNode(anyString(), anyString())).thenReturn(node);
-    when(portalContainer.getName()).thenReturn("portal");
-    when(portalContainer.getRestContextName()).thenReturn("rest");
-    when(repositoryService.getCurrentRepository()).thenReturn(repository);
-    when(repository.getConfiguration()).thenReturn(repositoryEntry);
-    when(sessionProviderService.getSystemSessionProvider(null)).thenReturn(sessionProvider);
-    when(repositoryEntry.getName()).thenReturn("repository");
-    when(sessionProvider.getSession(null,repository)).thenReturn(session);
-    when(node.getSession()).thenReturn(session);
-    Workspace workspace = mock(Workspace.class);
-    when(session.getWorkspace()).thenReturn(workspace);*/
     when(portalContainer.getName()).thenReturn("portal");
     when(portalContainer.getRestContextName()).thenReturn("rest");
     when(repositoryService.getCurrentRepository()).thenReturn(repository);
@@ -218,14 +243,14 @@ public class HTMLUploadImageProcessorTest {
     when(repositoryEntry.getName()).thenReturn("repository");
     when(wcmService.getReferencedContent(anyObject(), anyString(),anyString() )).thenReturn(node);
     when(node.getNode(anyString())).thenReturn(node);
+    when(node.getName()).thenReturn("image.png");
     when(node.getProperty(anyString())).thenReturn(property);
     when(property.getStream()).thenReturn(new ByteArrayInputStream("test data".getBytes()));
-    lenient().when(node.getPath()).thenReturn("/path/to/image.png");
 
     // When
     String processedContent = imageProcessor.processImagesForExport(content);
 
     // Then
-    assertTrue(processedContent.matches("<p>content with image: <img src=\"//-EXP_[a-z0-9]+.jpeg-//\" /></p>"));
+    assertTrue(processedContent.matches("<p>content with image: <img src=\"//-image.png-//\" /></p>"));
   }
 }
