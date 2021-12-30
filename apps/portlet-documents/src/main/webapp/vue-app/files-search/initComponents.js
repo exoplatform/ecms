5b5ad5427d75743c8967347611a970fc9575288f
Mohamed Amine Krout <mkrout@exoplatform.com>
2021-12-30 10:12:51 +0100

    TASK-51814: Add fav button to the search card
    * TASK-51814: Add fav button to the search card

--- apps/portlet-documents/src/main/webapp/vue-app/files-search/initComponents.js
+++ apps/portlet-documents/src/main/webapp/vue-app/files-search/initComponents.js
@@ -1,3 +1,6 @@
 import FileSearchCard  from './components/FileSearchCard.vue';
+import ExoDocsFavoriteAction from './components/ExoDocsFavoriteAction.vue';
+
 
 Vue.component('FileSearchCard', FileSearchCard);
+Vue.component('ExoDocsFavoriteAction', ExoDocsFavoriteAction);
