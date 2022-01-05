2fad384b5d76b6e5372279f8894a08a311168358
Montassar-Hamadi <92787967+Montassar-Hamadi@users.noreply.github.com>
2022-01-05 20:18:05 +0100

    Task-52369: cannot upload a new version of a document when its name includes the character "+". (#1588)
    Problem: Before this fix, it was impossible to upload a new version of the document in file explorer if the filename contains a +. when checking the existence of the file, the field currentFolder contains the filename, and the + is read as a space.
    
    Fix: This change use function encodeURIComponent for variable current folder, so that, the + is transformed to %2B and then in the rest service, we decode it with URLDecoder.decode, and we replace the parameter string "UTF-8" by StandardCharsets.UTF_8 to avoid errors in code smells, so that we are able to see the +.

--- apps/portlet-explorer/src/main/webapp/javascript/eXo/ecm/MultiUpload.js
+++ apps/portlet-explorer/src/main/webapp/javascript/eXo/ecm/MultiUpload.js
@@ -586,7 +586,7 @@
 		  var uri = eXo.ecm.MultiUpload.restContext + "/wcmDriver/uploadFile/checkExistence?" +
 		  "repositoryName=repository&workspaceName=" + eXo.ecm.MultiUpload.ws +
 		  "&driverName=" + eXo.ecm.MultiUpload.drive +
-		  "&currentFolder=" + eXo.ecm.MultiUpload.pathMap[id] +
+		  "&currentFolder=" + encodeURIComponent(eXo.ecm.MultiUpload.pathMap[id]) +
 		  "&currentPortal="+ eXo.ecm.MultiUpload.portalName +
 		  "&userId=" + eXo.ecm.MultiUpload.userId +
 		  "&fileName=" + cleanName(eXo.ecm.MultiUpload.uploadingFileIds[id].name) + 
@@ -817,7 +817,7 @@
 		  var uri = eXo.ecm.MultiUpload.restContext + "/wcmDriver/uploadFile/control?" +
 		  "repositoryName=repository&workspaceName=" + eXo.ecm.MultiUpload.ws +
 		  "&driverName=" + eXo.ecm.MultiUpload.drive +
-		  "&currentFolder=" + eXo.ecm.MultiUpload.pathMap[id] +
+		  "&currentFolder=" + encodeURIComponent(eXo.ecm.MultiUpload.pathMap[id]) +
 		  "&currentPortal="+ eXo.ecm.MultiUpload.portalName +
 		  "&userId=" + eXo.ecm.MultiUpload.userId +
 		  "&action=progress&uploadId=" + id;
@@ -905,7 +905,7 @@
 		    var uri = eXo.ecm.MultiUpload.restContext + "/wcmDriver/uploadFile/control?" +
 		    "repositoryName=repository&workspaceName=" + eXo.ecm.MultiUpload.ws +
 		    "&driverName=" + eXo.ecm.MultiUpload.drive +
-		    "&currentFolder=" + eXo.ecm.MultiUpload.pathMap[progressID] +
+		    "&currentFolder=" + encodeURIComponent(eXo.ecm.MultiUpload.pathMap[progressID]) +
 		    "&currentPortal="+ eXo.ecm.MultiUpload.portalName +
 		    "&userId=" + eXo.ecm.MultiUpload.userId +
 		    "&uploadId=" + progressID +
