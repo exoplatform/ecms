d0dfeb6a463c913e3f9b26420b6b41976a2d9a07
Mohamed Amine Krout <mkrout@exoplatform.com>
2022-01-28 10:52:40 +0100

    TASK-52638: Migration of old favorites (fix old  portlet display) (#1639)
    * TASK-52638: Migration of old favorites (fix old  portlet display)

--- core/services/src/main/java/org/exoplatform/services/cms/documents/impl/FavoriteServiceImpl.java
+++ core/services/src/main/java/org/exoplatform/services/cms/documents/impl/FavoriteServiceImpl.java
@@ -120,7 +120,7 @@ public class FavoriteServiceImpl implements org.exoplatform.services.cms.documen
 
   public boolean isFavoriter(String userName, Node node) throws Exception {
     Identity identity = identityManager.getOrCreateUserIdentity(userName);
-    Favorite favorite = new Favorite("file", node.getUUID(), "", Long.parseLong(identity.getId()));
+    Favorite favorite = new Favorite("file", ((NodeImpl) node).getIdentifier(), "", Long.parseLong(identity.getId()));
     return favoriteService.isFavorite(favorite);
   }
 
