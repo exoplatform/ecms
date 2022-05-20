fa69d90e67d5380b65fcf60f8e06b32ef0df2023
Azmi TOUIL <42934070+AzmiTouil@users.noreply.github.com>
2022-05-20 17:36:19 +0100

    Task-56988: Photo disappears when it is moved or resized in CKEditor (#1757)
    This PR will Update processUpload rest api to create folder when it doesn't exist
    + Move select image plugin to eXoWCMResources
    + Store uploaded images in document storage by ckeditor instead of process the storage by HTMLUploadImageProcessor service

---
+++ apps/resources-wcm/src/main/webapp/eXoPlugins/selectImage/lang/en.js
@@ -0,0 +1,22 @@
+/*
+Copyright (c) 2003-2017, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.md or http://ckeditor.com/license
+*/
+CKEDITOR.plugins.setLang( 'selectImage', 'en', {
+  alt: 'Alternative Text',
+  btnUpload: 'Send it to the Server',
+  captioned: 'Captioned image',
+  captionPlaceholder: 'Caption',
+  infoTab: 'Image Info',
+  lockRatio: 'Lock Ratio',
+  menu: 'Change Image',
+  insertImage: 'Insert image',
+  pathName: 'image',
+  pathNameCaption: 'caption',
+  resetSize: 'Reset Size',
+  resizer: 'Click and drag to resize',
+  title: 'Select image',
+  uploadTab: 'Upload',
+  urlMissing: 'Image source URL is missing.',
+  altMissing: 'Alternative text is missing.'
+} );
