d9b47bf3968378d2d2640137abb0212eb8263ba7
Helmi Akermi <70575401+hakermi@users.noreply.github.com>
2022-05-26 10:32:17 +0100

    TASK-57175: Add getDocumentsTransferRules function in transferRulesService.js (#1795)
    Prior to this change, No function yet was defined to get information about download and share transfer rules settings.
    This PR should add a dedicated fetch function to fetch the two settings values

--- apps/portlet-documents/src/main/webapp/js/transferRulesService.js
+++ apps/portlet-documents/src/main/webapp/js/transferRulesService.js
@@ -15,3 +15,16 @@ export function getTransfertRulesDownloadDocumentStatus() {
     }
   });
 }
+
+export function getDocumentsTransferRules() {
+  return fetch(`${eXo.env.portal.context}/${eXo.env.portal.rest}/transferRules/getTransfertRulesDocumentStatus`, {
+    credentials: 'include',
+    method: 'GET',
+  }).then(resp => {
+    if (!resp || !resp.ok) {
+      throw new Error('Response code indicates a server error');
+    } else {
+      return resp.json();
+    }
+  });
+}
