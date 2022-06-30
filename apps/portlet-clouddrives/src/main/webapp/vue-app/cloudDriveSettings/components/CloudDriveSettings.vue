4667d656f44a3548c680c6946527ffa0264d9558
Ahmed Ben Mbarek <92788696+abenmbarek@users.noreply.github.com>
2022-06-30 10:16:57 +0100

    Task-58505: Implement connecting the personal drives in user settings (#1811)
    After this PR, we will be able to connect with our personal drives with an other way using the user settings instead of the attachments drawer.

---
+++ apps/portlet-clouddrives/src/main/webapp/vue-app/cloudDriveSettings/components/CloudDriveSettings.vue
@@ -0,0 +1,47 @@
+<!--
+Copyright (C) 2022 eXo Platform SAS.
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+You should have received a copy of the GNU Affero General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+-->
+<template>
+  <v-app>
+    <v-card
+      class="border-radius ma-4"
+      flat>
+      <v-list>
+        <v-list-item>
+          <v-list-item-content>
+            <v-list-item-title class="title text-color">
+              {{ $t("cloudDriveSettings.label.title") }}
+            </v-list-item-title>
+            <v-list-item-subtitle class="my-3 text-color">
+              {{ $t("cloudDriveSettings.label.subtitle") }}
+            </v-list-item-subtitle>
+            <v-list-item-subtitle class="my-3 text-sub-title font-italic">
+              {{ $t("cloudDriveSettings.label.description") }}
+            </v-list-item-subtitle>
+          </v-list-item-content>
+          <v-list-item-action>
+            <v-btn
+              icon>
+              <em class="uiIconEdit uiIconLightBlue pb-2"></em>
+            </v-btn>
+          </v-list-item-action>
+        </v-list-item>
+      </v-list>
+    </v-card>
+  </v-app>
+</template>
+
+<script>
+export default {
+};
+</script>
