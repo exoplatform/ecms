be4f1c915ba1f88e499646a1cedd5a560cdadefa
Ahmed Ben Mbarek <abenmbarek@exoplatform.com>
2022-07-15 13:11:39 +0100

    open connect window using a popup

--- apps/portlet-clouddrives/src/main/webapp/js/clouddrives.js
+++ apps/portlet-clouddrives/src/main/webapp/js/clouddrives.js
@@ -274,13 +274,33 @@
             return initRequest(request);
         };
 
+        var connectDrivePopup = function(url, title, w, h) {
+            // Fixes dual-screen position                         Most browsers      Firefox
+            var dualScreenLeft = window.screenLeft !== undefined ? window.screenLeft : screen.left;
+            var dualScreenTop = window.screenTop !== undefined ? window.screenTop : screen.top;
+
+            width = window.innerWidth ? window.innerWidth : document.documentElement.clientWidth ? document.documentElement.clientWidth : screen.width;
+            height = window.innerHeight ? window.innerHeight : document.documentElement.clientHeight ? document.documentElement.clientHeight : screen.height;
+
+            var left = ((width / 2) - (w / 2)) + dualScreenLeft;
+            var top = ((height / 2) - (h / 2)) + dualScreenTop;
+            var newWindow = window.open(url, title, 'scrollbars=yes, width=' + w + ', height=' + h + ', top=' + top + ', left=' + left);
+
+            // Puts focus on the newWindow
+            if (window.focus) {
+                newWindow.focus();
+            }
+            return newWindow;
+        }
+
         var connectDrive = function(providerId, authURL) {
             var authWindow;
             var authService;
 
             if (authURL) {
                 // use user interaction for authentication
-                authWindow = window.open(authURL, "_blank");
+                //authWindow = window.open(authURL, "_blank", "width=600,height=600");
+                authWindow = connectDrivePopup(authURL, providerId, '600', '600');
             } else {
                 // function to call for auth using authURL from provider
                 authService = serviceGet;
