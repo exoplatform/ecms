2822663b06535705ae9cd9b3861763e78116a72d
Ayoub Zayati <ayoub.ezzayati@exoplatform.com>
2022-08-29 13:17:27 +0100

    update headers

--- core/services/src/main/java/org/exoplatform/services/cms/clouddrives/settings/CloudDriveUserSettingsRest.java
+++ core/services/src/main/java/org/exoplatform/services/cms/clouddrives/settings/CloudDriveUserSettingsRest.java
@@ -1,20 +1,19 @@
 /*
  * Copyright (C) 2022 eXo Platform SAS.
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Affero General Public License
- * as published by the Free Software Foundation; either version 3
- * of the License, or (at your option) any later version.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, see<http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-
 package org.exoplatform.services.cms.clouddrives.settings;
 
 import io.swagger.v3.oas.annotations.Operation;
