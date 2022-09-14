ef7036a84ff9ee06d8f4ec65683c3674e5e13346
Ayoub Zayati <ayoub.ezzayati@exoplatform.com>
2022-09-14 16:27:32 +0200

    fix: Update ckeditor customConfig from /commons-extension/eXoConfig.js to /ecmexplorer/javascript/eXo/ecm/ckeditorCustom/config.js - EXO-58725
    Prior to this change, commons custom ckeditor config is configured as customConfig in UIFormRichtextInput.java. We need to update it by ECMS custom ckeditor in order to use custom ECMS configurations.

--- core/webui/src/main/java/org/exoplatform/ecm/webui/form/UIDialogForm.java
+++ core/webui/src/main/java/org/exoplatform/ecm/webui/form/UIDialogForm.java
@@ -84,7 +84,6 @@ import org.exoplatform.services.log.ExoLogger;
 import org.exoplatform.services.log.Log;
 import org.exoplatform.services.wcm.core.NodetypeConstant;
 import org.exoplatform.services.wcm.utils.WCMCoreUtils;
-import org.exoplatform.webui.form.UIFormRichtextInput;
 import org.exoplatform.web.application.ApplicationMessage;
 import org.exoplatform.web.application.RequestContext;
 import org.exoplatform.webui.application.WebuiRequestContext;
