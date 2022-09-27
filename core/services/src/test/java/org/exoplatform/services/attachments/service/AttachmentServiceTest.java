260b68a9805f65ce20ae9428090e0f8523ea5eed
Ayoub Zayati <ayoub.ezzayati@exoplatform.com>
2022-09-27 17:23:15 +0200

    Fix tests

--- core/services/src/test/java/org/exoplatform/services/attachments/service/AttachmentServiceTest.java
+++ core/services/src/test/java/org/exoplatform/services/attachments/service/AttachmentServiceTest.java
@@ -1,23 +1,24 @@
 package org.exoplatform.services.attachments.service;
 
-import static org.mockito.ArgumentMatchers.*;
-import static org.mockito.Mockito.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.lenient;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Collections;
+import java.util.List;
 
-import javax.jcr.*;
+import javax.jcr.Node;
+import javax.jcr.Property;
+import javax.jcr.Session;
+import javax.jcr.Workspace;
 
-import org.exoplatform.services.attachments.dao.AttachmentDAO;
-import org.exoplatform.services.attachments.storage.AttachmentStorageImpl;
-import org.exoplatform.services.attachments.utils.Utils;
-import org.exoplatform.services.cms.documents.*;
-import org.exoplatform.services.cms.drives.ManageDriveService;
-import org.exoplatform.services.cms.link.LinkManager;
-import org.exoplatform.services.cms.link.NodeFinder;
-import org.exoplatform.services.jcr.core.ExtendedSession;
-import org.exoplatform.services.jcr.ext.hierarchy.NodeHierarchyCreator;
-import org.exoplatform.services.security.MembershipEntry;
-import org.junit.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
@@ -25,14 +26,30 @@ import org.mockito.runners.MockitoJUnitRunner;
 
 import org.exoplatform.commons.testing.BaseExoTestCase;
 import org.exoplatform.commons.utils.CommonsUtils;
-import org.exoplatform.component.test.*;
+import org.exoplatform.component.test.ConfigurationUnit;
+import org.exoplatform.component.test.ConfiguredBy;
+import org.exoplatform.component.test.ContainerScope;
+import org.exoplatform.services.attachments.dao.AttachmentDAO;
 import org.exoplatform.services.attachments.model.Attachment;
 import org.exoplatform.services.attachments.storage.AttachmentStorage;
+import org.exoplatform.services.attachments.storage.AttachmentStorageImpl;
+import org.exoplatform.services.attachments.utils.Utils;
+import org.exoplatform.services.cms.documents.DocumentEditorProvider;
+import org.exoplatform.services.cms.documents.DocumentService;
+import org.exoplatform.services.cms.documents.NewDocumentTemplate;
+import org.exoplatform.services.cms.documents.NewDocumentTemplateConfig;
+import org.exoplatform.services.cms.documents.NewDocumentTemplateProvider;
+import org.exoplatform.services.cms.drives.ManageDriveService;
+import org.exoplatform.services.cms.link.LinkManager;
+import org.exoplatform.services.cms.link.NodeFinder;
 import org.exoplatform.services.jcr.RepositoryService;
 import org.exoplatform.services.jcr.config.RepositoryEntry;
+import org.exoplatform.services.jcr.core.ExtendedSession;
 import org.exoplatform.services.jcr.core.ManageableRepository;
 import org.exoplatform.services.jcr.ext.app.SessionProviderService;
 import org.exoplatform.services.jcr.ext.common.SessionProvider;
+import org.exoplatform.services.jcr.ext.hierarchy.NodeHierarchyCreator;
+import org.exoplatform.services.security.MembershipEntry;
 import org.exoplatform.social.core.identity.model.Identity;
 import org.exoplatform.social.core.identity.provider.OrganizationIdentityProvider;
 import org.exoplatform.social.core.manager.IdentityManager;
@@ -83,7 +100,7 @@ public class AttachmentServiceTest extends BaseExoTestCase {
   @Mock
   LinkManager                 linkManager;
 
-  @Mock
+  @Mock(extraInterfaces = {ExtendedSession.class})
   Session session;
 
   @Before
@@ -156,9 +173,9 @@ public class AttachmentServiceTest extends BaseExoTestCase {
     Node node1 = mock(Node.class);
     Node nodeContent1 = mock(Node.class);
     Property property = mock(Property.class);
-    ExtendedSession extendedSession = mock(ExtendedSession.class);
-    when(extendedSession.getNodeByIdentifier(anyString())).thenReturn(node1);
     Workspace workSpace = mock(Workspace.class);
+    when(((ExtendedSession) session).getNodeByIdentifier(anyString())).thenReturn(node1);
+    when(session.getNodeByUUID(anyString())).thenReturn(node1);
     when(session.getWorkspace()).thenReturn(workSpace);
     lenient().when(node1.getSession()).thenReturn(session);
     lenient().when(node1.getProperty(anyString())).thenReturn(property);
@@ -167,12 +184,14 @@ public class AttachmentServiceTest extends BaseExoTestCase {
     lenient().when(property.getDate()).thenReturn(Calendar.getInstance());
     lenient().when(property.getLong()).thenReturn((long) 1);
     lenient().when(node1.getPath()).thenReturn("/collaboration/");
-    lenient().when(extendedSession.getNodeByIdentifier(String.valueOf(1))).thenReturn(node1);
+    lenient().when(session.getNodeByUUID(String.valueOf(1))).thenReturn(node1);
+    lenient().when(((ExtendedSession) session).getNodeByIdentifier(String.valueOf(1))).thenReturn(node1);
 
     Node node2 = mock(Node.class);
     Node nodeContent2 = mock(Node.class);
     Property property2 = mock(Property.class);
-    when(extendedSession.getNodeByIdentifier(anyString())).thenReturn(node2);
+    when(session.getNodeByUUID(anyString())).thenReturn(node2);
+    when(((ExtendedSession) session).getNodeByIdentifier(anyString())).thenReturn(node2);
     when(session.getWorkspace()).thenReturn(workSpace);
     lenient().when(node2.getSession()).thenReturn(session);
     lenient().when(node2.getProperty(anyString())).thenReturn(property2);
@@ -181,12 +200,14 @@ public class AttachmentServiceTest extends BaseExoTestCase {
     lenient().when(property2.getDate()).thenReturn(Calendar.getInstance());
     lenient().when(property2.getLong()).thenReturn((long) 2);
     lenient().when(node2.getPath()).thenReturn("/collaboration/");
-    lenient().when(extendedSession.getNodeByIdentifier(String.valueOf(2))).thenReturn(node2);
+    lenient().when(session.getNodeByUUID(String.valueOf(2))).thenReturn(node2);
+    lenient().when(((ExtendedSession) session).getNodeByIdentifier(String.valueOf(2))).thenReturn(node2);
 
     Node node3 = mock(Node.class);
     Node nodeContent3 = mock(Node.class);
     Property property3 = mock(Property.class);
-    when(extendedSession.getNodeByIdentifier(anyString())).thenReturn(node3);
+    when(session.getNodeByUUID(anyString())).thenReturn(node3);
+    when(((ExtendedSession) session).getNodeByIdentifier(anyString())).thenReturn(node3);
     lenient().when(session.getWorkspace()).thenReturn(workSpace);
     lenient().when(node3.getSession()).thenReturn(session);
     lenient().when(node3.getProperty(anyString())).thenReturn(property3);
@@ -195,7 +216,8 @@ public class AttachmentServiceTest extends BaseExoTestCase {
     lenient().when(property3.getDate()).thenReturn(Calendar.getInstance());
     lenient().when(property3.getLong()).thenReturn((long) 3);
     lenient().when(node3.getPath()).thenReturn("/collaboration/");
-    Mockito.when(extendedSession.getNodeByIdentifier(String.valueOf(3))).thenReturn(node3);
+    when(session.getNodeByUUID(String.valueOf(3))).thenReturn(node3);
+    when(((ExtendedSession) session).getNodeByIdentifier(String.valueOf(3))).thenReturn(node3);
 
     String username = "testuser";
     long currentIdentityId = 2;
@@ -297,7 +319,7 @@ public class AttachmentServiceTest extends BaseExoTestCase {
     lenient().when(node1.getPath()).thenReturn("/collaboration/");
     lenient().when(node1.getName()).thenReturn(docTitle);
     lenient().when(node1.getUUID()).thenReturn(createdDocUUID);
-    lenient().when(session.getNodeByUUID(createdDocUUID)).thenReturn(node1);
+    lenient().when(((ExtendedSession) session).getNodeByIdentifier(createdDocUUID)).thenReturn(node1);
     lenient().when(Utils.getParentFolderNode(session, manageDriveService,nodeHierarchyCreator, nodeFinder, pathDrive, docPath)).thenReturn(parentNode);
     lenient().when(documentService.createDocumentFromTemplate(parentNode, docTitle, documentTemplate)).thenReturn(node1);
     lenient().when(documentService.getNewDocumentTemplateProviders()).thenReturn(Collections.singletonList(documentTemplateProvider));
