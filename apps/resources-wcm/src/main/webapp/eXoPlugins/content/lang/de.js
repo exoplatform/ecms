bef6ad82950d00676b1cc768cef0abfc999be33e
jihed <jihed@exoplatform.com>
2023-04-17 10:34:57 +0100

    fix:Add missing german transaltion in Ckeditor

---
+++ apps/resources-wcm/src/main/webapp/eXoPlugins/content/lang/de.js
@@ -0,0 +1,56 @@
+var UserLanguage = {
+	Filter							:	"Filter",
+	All									:"Alles",
+	WebContents						:   "Web-Inhalte",
+	DMSDocuments					:	"Document DMS",
+	Medias							:	"Medien",
+	UploadFile						:	"Eine Datei hochladen",
+
+	SettingTitle					: 	"Einstellungen",
+	SetView							: 	"Anzeige",
+	SetSort							: 	"Sortieren",
+
+	SortName						:	"Dateiname",
+	SortDate						: 	"Datum",
+	SortSize						: 	"Grösse",
+
+	ViewThumbnail						: 	"Thumbnail",
+	ViewList						: 	"Liste",
+
+	ViewThumbnailLabel  					:	"Thumbnailansicht",
+	ViewListLabel       					:	"Liste ansehen",
+
+	Home							:	"Startseite",
+
+	FileName						:	"Name",
+	CreateDate						:	"Datum",
+	FileSize						:	"Grösse",
+
+	NoContent						:	"Es ist kein Inhalt vorhanden",
+
+	BtnCancel						: 	"abbrechen",
+	BtnUpload						: 	"senden",
+	BtnSetting						: 	"Einstellung",
+	BtnClose						: 	"schliessen",
+	BtnSave							: 	"speichern",
+	BtnAbort						: 	"abbrechen",
+	BtnDelete						: 	"löschen",
+
+	LabelFolder						: 	"Datei",
+	LabelAlert						: 	"Bitte wählen Sie eine Datei zum hochladen aus",
+	LabelName						: 	"Name",
+	SelectFile						:	"Datei",
+
+	//Document Auto Versioning
+	DocumentAuto_label_existing:"Existierende Datei",
+	DocumentAuto_label_keepBoth:"Beide Dateien behalten",
+	DocumentAuto_label_createVersion:"Eine neue Version erstellen",
+	DocumentAuto_label_replace:"Ersetzen",
+	DocumentAuto_label_or:"oder",
+	DocumentAuto_label_cancel:"abbrechen"
+}
+
+CKEDITOR.plugins.setLang( 'content', 'de',
+{
+  WCMInsertContentPlugins        : 'Einen Link einfügen'
+} );
