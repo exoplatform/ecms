0393382004d254a086716346cb3410d3c875b737
jihed <jihed@exoplatform.com>
2023-04-18 11:24:25 +0100

    edit discalimer and fix sonar reported code smell

--- apps/resources-wcm/src/main/webapp/eXoPlugins/attachFile/lang/de.js
+++ apps/resources-wcm/src/main/webapp/eXoPlugins/attachFile/lang/de.js
@@ -1,6 +1,18 @@
 /*
- * Copyright (c) 2003-2017, CKSource - Frederico Knabben. All rights reserved.
- * For licensing, see LICENSE.md or http://ckeditor.com/license
+ * Copyright (C) 2023 eXo Platform SAS.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License
+ * as published by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 CKEDITOR.plugins.setLang( 'attachFile', 'de',
     {
