c0530b238e7cba3b885c0cc8f130625338c9dc64
jihed <jihed@exoplatform.com>
2023-04-18 10:52:25 +0100

    Add discalimer and fix sonar reported code smell

--- apps/resources-wcm/src/main/webapp/eXoPlugins/content/lang/de.js
+++ apps/resources-wcm/src/main/webapp/eXoPlugins/content/lang/de.js
@@ -1,4 +1,8 @@
-var UserLanguage = {
+/*
+Copyright (c) 2003-2017, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.md or http://ckeditor.com/license
+*/
+let UserLanguage = {
 	Filter							:	"Filter",
 	All									:"Alles",
 	WebContents						:   "Web-Inhalte",
