9861627b926b3fe6bde528a9d057554ce7297464
jihed <jihed@exoplatform.com>
2023-04-18 10:45:30 +0100

    Add discalimer to fix sonar reported code smell

--- apps/resources-wcm/src/main/webapp/eXoPlugins/insertPortalLink/lang/de.js
+++ apps/resources-wcm/src/main/webapp/eXoPlugins/insertPortalLink/lang/de.js
@@ -1,3 +1,7 @@
+/*
+Copyright (c) 2003-2017, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.md or http://ckeditor.com/license
+*/
 CKEDITOR.plugins.setLang( 'insertPortalLink', 'de',
 {
   WCMInsertPortalLinkPlugins        : 'Link in eine Seite einfÃ¼gen',
