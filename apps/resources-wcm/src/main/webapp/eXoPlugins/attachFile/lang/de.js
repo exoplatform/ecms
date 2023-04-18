ac1e04fdbb2fdcab31215feb5375f78df8644af1
jihed <jihed@exoplatform.com>
2023-04-18 11:02:01 +0100

    edit discalimer and fix sonar reported code smell

--- apps/resources-wcm/src/main/webapp/eXoPlugins/attachFile/lang/de.js
+++ apps/resources-wcm/src/main/webapp/eXoPlugins/attachFile/lang/de.js
@@ -1,7 +1,7 @@
 /*
-Copyright (c) 2003-2017, CKSource - Frederico Knabben. All rights reserved.
-For licensing, see LICENSE.md or http://ckeditor.com/license
-*/
+ * Copyright (c) 2003-2017, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or http://ckeditor.com/license
+ */
 CKEDITOR.plugins.setLang( 'attachFile', 'de',
     {
       buttonTooltip                 : 'Fügen Sie ein Dokument oder ein Bild hinzu',
