5125f0b8a2bf901b8a946790f0ec4c5023328d00
Jihed Chabbeh <31882754+Jihed525@users.noreply.github.com>
2024-03-29 14:23:03 +0100

    fix: upload existing documents + infinite loading - EXO-69948
    Before this change, when In spaceX Doc app upload files twice, on the second drawer displays infinite loading even if choice selected and times only one uploaded file is displayed on drawer. After this change, the contents of the drawer are displayed correctly.
    
    (cherry picked from commit 831fa2f110195ddd83d5e6ce32e07b4b336f6764)
    (cherry picked from commit 3ca8ab4f9ed56df5b15daadbd57c01c352396a32)
    (cherry picked from commit 0c36b03b05e5436308073aaf3110ccd85ad9710c)
    (cherry picked from commit ff720d1c244e6f492efcf02cb1c0c0497be6e25a)

--- apps/portlet-documents/src/main/webapp/vue-app/attachment/components/attachments-upload-components/AttachmentsUploadInput.vue
+++ apps/portlet-documents/src/main/webapp/vue-app/attachment/components/attachments-upload-components/AttachmentsUploadInput.vue
@@ -231,7 +231,7 @@ export default {
         if (actions.length > 0) {
           file.actions = actions;
           file.waitAction = true;
-          this.$root.$emit('alert-message', this.$t('attachments.upload.conflict.message'), 'warning');
+          this.$root.$emit('alert-message', this.$root.$t('attachments.upload.conflict.message'), 'warning');
           this.$root.$emit('start-loading-attachment-drawer');
         }
         this.$root.$emit('add-new-uploaded-file', file);
