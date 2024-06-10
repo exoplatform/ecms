d5a0985129cf10b97a3778668a019d82a7717723
Helmi Akermi <70575401+hakermi@users.noreply.github.com>
2024-06-10 10:40:13 +0100

    fix: Fix resizing images and alignment via the selectImage dialog and toolbar options - EXO-70243,EXO-72349
    Fix resizing images and alignment via the selectImage dialog and toolbar options

--- apps/resources-wcm/src/main/webapp/eXoPlugins/selectImage/dialogs/selectImage.js
+++ apps/resources-wcm/src/main/webapp/eXoPlugins/selectImage/dialogs/selectImage.js
@@ -171,7 +171,7 @@ CKEDITOR.dialog.add( 'selectImage', function( editor ) {
                         let imageLink = $(dialog.getElement().$).find(".imageLinkArea input").val();
                         const imageLinkTarget = $(dialog.getElement().$).find(".imageLinkTargetArea select").find(":selected").val();
                         widget.setData( 'src', $imageElement.attr("src") );
-                        widget.setData( 'alt', $imageElement.attr("alt") );
+                        widget.setData( 'alt', $imageElement.attr("alt") || '');
                         if (imageLink && !UrlProtocolRegex.test(imageLink)) {
                           imageLink = 'https://' + imageLink;
                         }
@@ -186,13 +186,13 @@ CKEDITOR.dialog.add( 'selectImage', function( editor ) {
                           widget.setData('href', imageLink);
                           widget.setData('target', imageLinkTarget);
                         } else {
-                          widget.setData('link', null);
+                          widget.setData('link', undefined);
                         }
-                        if ($imageElement.hasClass("left")) {
+                        if ($imageElement.hasClass("pull-left")) {
                           widget.setData( 'align', 'left' );
-                        } else if ($imageElement.hasClass("right")) {
+                        } else if ($imageElement.hasClass("pull-right")) {
                           widget.setData( 'align', 'right' );
-                        } else if ($imageElement.hasClass("center")) {
+                        } else if ($imageElement.hasClass("text-center")) {
                           widget.setData( 'align', 'center' );
                         }
                       });
