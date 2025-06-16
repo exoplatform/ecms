cae988ea4b5fbd0c3a161b60305436c57e437d24
Sofien Haj Chedhli <shajchedli@exoplatform.com>
2025-06-16 11:56:07 +0100

    feat: eXIP13: Review files search result display - EXO-79550 (#2494)

--- apps/portlet-documents/src/main/webapp/vue-app/files-search/main.js
+++ apps/portlet-documents/src/main/webapp/vue-app/files-search/main.js
@@ -12,6 +12,34 @@ if (extensionRegistry) {
 
 Vue.use(Vuetify);
 
-export function formatSearchResult(result) {
-  return result;
+export async function formatSearchResult(results, term) {
+  if (results?.length) {
+    return await Promise.all(results.map(async (file) => {
+      const nodePath = file?.nodePath || '';
+
+      if (file?.drive !== 'Private') {
+        const spacesPrefix = '/spaces/';
+        const spaceIndex = nodePath.indexOf(spacesPrefix);
+        if (spaceIndex !== -1) {
+          const spaceGroupId = nodePath.substring(spaceIndex + spacesPrefix.length).split('/')[0];
+          try {
+            file.space = await Vue.prototype.$spaceService.getSpaceByGroupId(spaceGroupId);
+          } catch (error) {
+            console.error(`Failed to fetch space for groupId "${spaceGroupId}":`, error);
+          }
+        }
+      } else {
+        const privateIndex = nodePath.indexOf('/Private/');
+        if (privateIndex !== -1) {
+          const authorPath = nodePath.substring(0, privateIndex);
+          file.author = authorPath.substring(authorPath.lastIndexOf('/') + 1);
+        }
+      }
+
+      file.title = file.title.replace(new RegExp(`(${term})`, 'ig'), '<span class="searchMatchExcerpt">$1</span>');
+      file.name = nodePath.substring(nodePath.lastIndexOf('/') + 1);
+      return file;
+    }));
+  }
+  return results;
 }
