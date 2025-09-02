e59f35b4e8101422df7c173352f4d219d3c8944e
Mohamed Amine Krout <mkrout@exoplatform.com>
2025-09-02 11:40:10 +0100

    fix: Remove any dependencies to old doc preview - EXO-80993
    This change will cleanup dependencies of the old doc preview

--- apps/portlet-documents/src/main/webapp/vue-app/documents/initComponents.js
+++
@@ -1,9 +0,0 @@
-import Documents from './components/Documents.vue';
-
-const components = {
-  'documents': Documents,
-};
-
-for (const key in components) {
-  Vue.component(key, components[key]);
-}
